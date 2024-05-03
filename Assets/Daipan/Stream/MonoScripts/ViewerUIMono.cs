857b5c27a67fb82a0e3a09ca1f78a1ea80f357f5
suupia <morita777k@icloud.com>
2024-05-03 20:05:21 +0900

    Add : Text to show streamStatus

--- Assets/Daipan/Stream/MonoScripts/ViewerUIMono.cs
+++ Assets/Daipan/Stream/MonoScripts/ViewerUIMono.cs
@@ -9,18 +9,27 @@ namespace Stream.Viewer.MonoScripts
     public class ViewerUIMono : MonoBehaviour
     {
         [SerializeField] TextMeshProUGUI viewerNumberText = null!;
+        [SerializeField] TextMeshProUGUI isExcitingText = null!;
+        [SerializeField] TextMeshProUGUI existIrrationalFactorsText = null!;
 
+        StreamStatus _streamStatus = null!;
         ViewerNumber _viewerNumber = null!;
 
         void Update()
         {
-            viewerNumberText.text = _viewerNumber.Number.ToString();
+            viewerNumberText.text = $"Current Viewers : {_viewerNumber.Number}";
+            isExcitingText.text = $"IsExciting : {_streamStatus.IsExciting}";
+            existIrrationalFactorsText.text = $"ExistIrrationalFactors : {_streamStatus.ExistIrrationalFactors}";
         }
 
+
         [Inject]
-        public void Initialize(ViewerNumber viewerNumber)
+        public void Initialize(
+            ViewerNumber viewerNumber,
+            StreamStatus streamStatus)
         {
             _viewerNumber = viewerNumber;
+            _streamStatus = streamStatus;
         }
     }
 }
