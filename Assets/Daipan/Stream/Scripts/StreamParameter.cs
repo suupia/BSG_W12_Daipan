2f9218daa7fcfa23e53a7f9581edcbaa8318c624
suupia <morita777k@icloud.com>
2024-05-03 20:57:04 +0900

    Add : Header to explain variables

--- Assets/Daipan/Stream/Scripts/StreamParameter.cs
+++ Assets/Daipan/Stream/Scripts/StreamParameter.cs
@@ -8,14 +8,18 @@ namespace Stream.Viewer.Scripts
     [Serializable]
     public sealed class ViewerParameter
     {
+        [Header("一秒間に増える視聴者の数")]
         public int increaseNumberPerSecond;
+        [Header("理不尽な状態の時の一秒間に減る視聴者の数")]
         public int decreaseNumberWhenIrradiated;
     }
 
     [Serializable]
     public sealed class DaipanParameter
     {
+        [Header("台パンによって増える視聴者の数")]
         public int increaseNumberByDaipan;
+        [Header("盛り上がっているときに増える視聴者の数")]
         public int increaseNumberWhenExciting;
     }
 
