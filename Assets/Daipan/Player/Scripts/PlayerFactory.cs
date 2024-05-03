1dfb340b1390991767f12e3e520efd41c62cffb0
suupia <morita777k@icloud.com>
2024-05-03 16:23:14 +0900

    Add : PlayerParameter.cs as ScriptableObjects

--- Assets/Daipan/Player/Scripts/PlayerFactory.cs
+++ Assets/Daipan/Player/Scripts/PlayerFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Daipan.Utility;
+using Unity.VisualScripting;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -8,25 +9,26 @@ namespace Daipan.Player.Scripts
     public class PlayerFactory : IStartable
     {
         readonly IObjectResolver _container;
-        readonly IPrefabLoader<PlayerMono> _loader;
+        readonly IPrefabLoader<PlayerMono> _playerMonoLoader;
 
         [Inject]
         public PlayerFactory(
             IObjectResolver container,
-            IPrefabLoader<PlayerMono> loader)
+            IPrefabLoader<PlayerMono> playerMonoLoader)
         {
             _container = container;
-            _loader = loader;
+            _playerMonoLoader = playerMonoLoader;
+
         }
 
         void IStartable.Start()
         {
            // PlayerMonoのプレハブをロードして生成 
-           var playerMonoPrefab = _loader.Load(); 
+           var playerMonoPrefab = _playerMonoLoader.Load(); 
            // IObjectResolverを使ってPlayerMonoを生成すると依存関係が解決される
            var playerMono = _container.Instantiate(playerMonoPrefab);
-
-        }
+           
+         }
 
     }
 }
--- Assets/Daipan/Player/Tests/PlayerTestScope.cs
+++ Assets/Daipan/Player/Tests/PlayerTestScope.cs
@@ -1,5 +1,6 @@
 using Daipan.Player.Scripts;
 using Daipan.Utility;
+using Unity.VisualScripting;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -7,8 +8,13 @@ using VContainer.Unity;
 // プレイヤーの処理をテストするためのLifetimeScope
 public class PlayerTestScope : LifetimeScope
 {
+    [SerializeField] PlayerParameter playerParameter;
+    
     protected override void Configure(IContainerBuilder builder)
     {
+        // ScriptableObjectのPlayerParameterを登録
+        builder.RegisterInstance(playerParameter.attackParameter);
+        
         // Playerのプレハブをロードするクラスを登録
         builder.Register<PlayerPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<PlayerMono>>();
        
