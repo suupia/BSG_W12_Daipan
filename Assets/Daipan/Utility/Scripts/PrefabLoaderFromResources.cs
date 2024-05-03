33d29e27fcb6c621baff6b2dcef9e643bfe8c522
suupia <morita777k@icloud.com>
2024-05-03 11:57:51 +0900

    Add : PrefabLoaderFromResources.cs

--- Assets/Daipan/Utility/Scripts/PrefabLoaderFromResources.cs
+++ Assets/Daipan/Utility/Scripts/PrefabLoaderFromResources.cs
@@ -6,22 +6,19 @@ namespace Daipan.Utility.Scripts
     // Other classes can be created by implementing IPrefabLoader, such as PrefabLoaderFromAssetBundle and PrefabLoaderFromStreamingAssets.
     public class PrefabLoaderFromResources<T> : IPrefabLoader<T> where T : Object
     {
-        readonly string _folderPath;
-        readonly string _prefabName;
+        readonly string _path;
 
-        public PrefabLoaderFromResources(string folderPath, string prefabName)
+        public PrefabLoaderFromResources(string path)
         {
-            _folderPath = folderPath;
-            _prefabName = prefabName;
+            _path = path;
         }
 
         public T Load()
         {
-            var result = Resources.Load<T>(_folderPath + "/" + _prefabName);
+            var result = Resources.Load<T>(_path);
             if (result == null)
             {
-                Debug.LogError(
-                    $"Failed to load prefab. folderPath ={_folderPath + "/" + _prefabName} prefabName = {_prefabName}");
+                Debug.LogError($"Failed to load prefab. Path = {_path}");
                 return null;
             }
 
