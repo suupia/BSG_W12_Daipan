3ad9b9ae36c69481f3036a20bee1b10babee11eb
suupia <morita777k@icloud.com>
2024-05-03 20:26:35 +0900

    Add : Buttons to control streamStatus

--- Assets/Daipan/Stream/Tests/PlayerTestInput.cs
+++ Assets/Daipan/Stream/Tests/PlayerTestInput.cs
@@ -7,28 +7,29 @@ namespace Stream.Viewer.Tests
 {
     public class PlayerTestInput : MonoBehaviour
     {
+        [SerializeField] CustomButton isExcitedButton;
+        [SerializeField] CustomButton isIrritatedButton;
         DaipanExecutor _daipanExecutor = null!;
         StreamStatus _streamStatus = null!;
+
+        void Awake()
+        {
+            isExcitedButton.AddListener(() =>
+            {
+                _streamStatus.IsExciting = !_streamStatus.IsExciting;
+                Debug.Log("IsExciting : " + _streamStatus.IsExciting);
+            });
+
+            isIrritatedButton.AddListener(() =>
+            {
+                _streamStatus.ExistIrrationalFactors = !_streamStatus.ExistIrrationalFactors;
+                Debug.Log("ExistIrrationalFactors : " + _streamStatus.ExistIrrationalFactors);
+            });
+        }
 
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space)) _daipanExecutor.DaiPan();
-
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                if (_streamStatus.IsExciting)
-                    _streamStatus.IsExciting = false;
-                else
-                    _streamStatus.IsExciting = true;
-            }
-
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                if (_streamStatus.ExistIrrationalFactors)
-                    _streamStatus.ExistIrrationalFactors = false;
-                else
-                    _streamStatus.ExistIrrationalFactors = true;
-            }
         }
 
         [Inject]
