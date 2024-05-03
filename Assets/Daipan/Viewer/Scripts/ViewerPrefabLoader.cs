66cc1b69508209eb525a346e9bb2dd52badc0112
suupia <morita777k@icloud.com>
2024-05-03 18:05:35 +0900

    Add : ViewerPrefabLoader.cs

---
+++ Assets/Daipan/Viewer/Scripts/ViewerPrefabLoader.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+using Daipan.Utility.Scripts;
+
+namespace Daipan.Viewer.Scripts
+{
+    public class ViewerPrefabLoader
+    {
+        readonly PrefabLoaderFromResources<ViewerMono> _loader;
+
+        public ViewerPrefabLoader()
+        {
+            _loader = new PrefabLoaderFromResources<ViewerMono>("Viewer");
+        }
+
+        public ViewerMono Load()
+        {
+            return _loader.Load();
+        }
+    }
+}
