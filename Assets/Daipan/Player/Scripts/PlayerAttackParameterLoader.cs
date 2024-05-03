4066e4dced3052bfb3b12e4bc3b9cef06fe06ec7
suupia <morita777k@icloud.com>
2024-05-03 15:25:23 +0900

    Add : PlayerAttackParameterLoader.cs

---
+++ Assets/Daipan/Player/Scripts/PlayerAttackParameterLoader.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+using Daipan.Utility;
+using Daipan.Utility.Scripts;
+
+namespace Daipan.Player.Scripts
+{
+
+    public class PlayerAttackParameterLoader : IPrefabLoader<PlayerMono>
+    {
+        readonly PrefabLoaderFromResources<PlayerMono> _loader;
+     
+
+        public PlayerAttackParameterLoader()
+        {
+            _loader = new PrefabLoaderFromResources<PlayerMono>("Player");
+        }
+        
+        public PlayerMono Load()
+        {
+            return _loader.Load();
+        }
+    }
+
+}
