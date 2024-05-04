b6425e00269e2200939d8fff514d6351ff738120
aya097 <akase097@gmail.com>
2024-05-05 00:27:54 +0900

    add : EnemyParameter
    現在EnemyAttackParameter、EnemyHPParameter、EnemyMovementParameterを含んでいます

---
+++ Assets/Daipan/Enemy/Scripts/EnemyParameter.cs
@@ -0,0 +1,36 @@
+#nullable enable
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Enemy
+{
+    [Serializable]
+    public sealed class EnemyAttackParameter
+    {
+        public int AttackAmount;
+    }
+
+    [Serializable]
+    public sealed class EnemyHPParameter
+    {
+        public int HPAmount;
+    }
+
+    [Serializable]
+    public sealed class EnemyMovementParameter
+    {
+        public float MovementAmount;
+    }
+
+
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/EnemyParameter", order = 1)]
+    public sealed class EnemyParameter : ScriptableObject
+    {
+        public EnemyAttackParameter attackParameter = null;
+        public EnemyHPParameter hpParameter = null;
+        public EnemyMovementParameter movementParameter = null;
+    }
+}
