c77546848eeee95b99e15dbb8cc8144f01961f8c
aya097 <akase097@gmail.com>
2024-05-05 00:35:56 +0900

    add : EnemyAttributeParameters.cs

---
+++ Assets/Daipan/Enemy/Scripts/EnemyAtrtibuteParameters.cs
@@ -0,0 +1,14 @@
+#nullable enable
+using System;
+using UnityEngine;
+
+
+namespace Enemy
+{
+    
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/EnemyAttributeParameters", order = 1)]
+    public sealed class EnemyAttributeParameters : ScriptableObject
+    {
+        
+    }
+}
