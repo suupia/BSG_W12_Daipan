ba9f8e9632a8dd6fd56a4ce8266feb6bb44ad823
aya097 <akase097@gmail.com>
2024-05-06 21:02:25 +0900

    add : EnemyMonoに[Inject]を追加

--- Assets/Daipan/Enemy/Interfaces/IEnemyOnHit.cs
+++ Assets/Daipan/Enemy/Interfaces/IEnemyOnHit.cs
@@ -1,11 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace Enemy
 {
-
-
     public interface IEnemyOnHit
     {
         public void OnHit();
--- Assets/Daipan/Enemy/MonoScripts/EnemyMono.cs
+++ Assets/Daipan/Enemy/MonoScripts/EnemyMono.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using Enemy;
 using UnityEngine;
+using VContainer;
 
 public class EnemyMono : MonoBehaviour
 {
@@ -12,5 +11,12 @@ public class EnemyMono : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A)) _enemyAttack.Attack();
         if (Input.GetKeyDown(KeyCode.S)) _enemyOnHitNormal.OnHit();
+    }
+
+    [Inject]
+    public void Initialize(EnemyAttack enemyAttack, EnemyOnHitNormal enemyOnHitNormal)
+    {
+        _enemyAttack = enemyAttack;
+        _enemyOnHitNormal = enemyOnHitNormal; 
     }
 }
