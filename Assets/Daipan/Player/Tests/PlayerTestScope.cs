f69137f68aa5f03fa4504f85fea29b5a2cd27592
suupia <morita777k@icloud.com>
2024-05-06 23:14:37 +0900

    Rename : ~Factory to ~Spawner

--- Assets/Daipan/Player/Tests/PlayerTestScope.cs
+++ Assets/Daipan/Player/Tests/PlayerTestScope.cs
@@ -20,12 +20,12 @@ public class PlayerTestScope : LifetimeScope
        
         // Playerの生成を行うクラスを登録（今後様々なPlayerを作れるようにFactoryパターンを採用）
         builder.Register<PlayerAttack>(Lifetime.Scoped);
-        builder.Register<PlayerFactory>(Lifetime.Scoped);
+        builder.Register<PlayerSpawner>(Lifetime.Scoped);
 
 
         builder.UseEntryPoints(Lifetime.Singleton, entryPoints =>
         {
-            entryPoints.Add<PlayerFactory>();
+            entryPoints.Add<PlayerSpawner>();
         });
     }
 }
