9dbcb3cb6b369f8bebd91eed2c2a5c4daf10b32a
aya097 <akase097@gmail.com>
2024-05-06 20:57:10 +0900

    add : EnemyFactoryに機能を追加

--- Assets/Daipan/Enemy/Scripts/EnemyFactory.cs
+++ Assets/Daipan/Enemy/Scripts/EnemyFactory.cs
@@ -1,12 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using Stream.Utility;
-using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
 
 namespace Enemy
 {
-    public class EnemyFactory
+    public class EnemyFactory : IStartable
     {
-        readonly IPrefabLoader<EnemyMono> _enemyMonoloader;
+        readonly IObjectResolver _container;
+        readonly IPrefabLoader<EnemyMono> _enemyMonoLoader;
+
+        [Inject]
+        public EnemyFactory(
+            IObjectResolver container,
+            IPrefabLoader<EnemyMono> enemyMonoLoader)
+        {
+            _container = container;
+            _enemyMonoLoader = enemyMonoLoader;
+        }
+
+        void IStartable.Start()
+        {
+            var enemyMonoPrefab = _enemyMonoLoader.Load();
+            var playerMono = _container.Instantiate(enemyMonoPrefab);
+        }
     }
 }
