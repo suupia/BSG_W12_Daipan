d9ebc1affc86584eae5aee747c0198c7f8266493
aya097 <akase097@gmail.com>
2024-05-11 00:28:45 +0900

    add : SpawnEnemy in EnemySpawner

--- Assets/Daipan/Enemy/Scripts/EnemySpawner.cs
+++ Assets/Daipan/Enemy/Scripts/EnemySpawner.cs
@@ -1,5 +1,8 @@
 #nullable enable
+using System;
+using System.Collections.Generic;
 using Stream.Utility;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
@@ -12,6 +15,9 @@ namespace Enemy
         readonly IPrefabLoader<EnemyMono> _enemyMonoLoader;
         readonly EnemyAttributeParameters _attributeParameters;
 
+        readonly Dictionary<ENEMY_TYPE, EnemyParameter> _enemyParameters = new();
+        private EnemyMono _enemyMonoPrefab;
+
         [Inject]
         public EnemySpawner(
             IObjectResolver container,
@@ -22,12 +28,24 @@ namespace Enemy
             _enemyMonoLoader = enemyMonoLoader;
             _attributeParameters = attributeParameters;
 
+            foreach (var enemyParam in _attributeParameters.enemyParameters)
+            {
+                if(_enemyParameters.ContainsKey(enemyParam.enemyType)) continue;
+                _enemyParameters.Add(enemyParam.enemyType,enemyParam);
+            }
         }
 
         void IStartable.Start()
         {
-            var enemyMonoPrefab = _enemyMonoLoader.Load();
+            _enemyMonoPrefab = _enemyMonoLoader.Load();
+            //Debug.Log(string.Join("\n", _attributeParameters.enemyParameters));
+            SpawnEnemy(ENEMY_TYPE.A_Type);
         }
 
+
+        void SpawnEnemy(ENEMY_TYPE enemyType)
+        {
+            var enemyObject = _container.Instantiate(_enemyMonoPrefab);
+        }
     }
 }
