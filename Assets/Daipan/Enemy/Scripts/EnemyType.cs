57c8c0c12d37d7f04e5ef5a2d5afd2401c3d9900
suupia <morita777k@icloud.com>
2024-05-14 23:49:18 +0900

    Add EnemyEnum

--- Assets/Daipan/Enemy/Scripts/EnemyType.cs
+++ Assets/Daipan/Enemy/Scripts/EnemyType.cs
@@ -1,4 +1,8 @@
 #nullable enable
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
 namespace Daipan.Enemy.Scripts
 {
     public enum EnemyType
@@ -8,5 +12,60 @@ namespace Daipan.Enemy.Scripts
         A,
         S,
         Cheetah
+    }
+    
+    public struct EnemyEnum
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsBoss { get; set; }
+
+        EnemyEnum(int id, string name, bool isBoss = false) => 
+            (Id, Name,IsBoss) = (id, name, isBoss);
+        
+        public static IEnumerable<EnemyEnum> GetAll() =>
+            typeof(EnemyEnum).GetFields(BindingFlags.Public |
+                                              BindingFlags.Static |
+                                              BindingFlags.DeclaredOnly)
+                .Where(field => field.FieldType == typeof(EnemyEnum))
+                .Select(field => (EnemyEnum)field.GetValue(null));
+
+        public static EnemyEnum None = new EnemyEnum(0, "None");
+        public static EnemyEnum W = new EnemyEnum(1, "W");
+        public static EnemyEnum A = new EnemyEnum(2, "A");
+        public static EnemyEnum S = new EnemyEnum(3, "S");
+        public static EnemyEnum Cheetah = new EnemyEnum(4, "Cheetah", true);
+
+        #region Overrides
+
+        public static bool operator ==(EnemyEnum a, EnemyEnum b)
+        {
+            return a.Id == b.Id && a.Name == b.Name;
+        }
+
+        public static bool operator !=(EnemyEnum a, EnemyEnum b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is EnemyEnum enemyEnum)
+            {
+                return this == enemyEnum;
+            } 
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^ Name.GetHashCode();
+        }
+
+        public override string ToString() => Name;
+
+        #endregion
+
+
     }
 }
