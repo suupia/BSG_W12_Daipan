6b01af55a4e3bb64a068e570e147857ac11b0884
suupia <suupia771@gmail.com>
2024-05-15 20:28:26 +0900

    Fix CommentSpawner.cs to add commentMono

--- Assets/Daipan/Comment/MonoScripts/CommentSpawner.cs
+++ Assets/Daipan/Comment/MonoScripts/CommentSpawner.cs
@@ -9,28 +9,33 @@ namespace Daipan.Comment.MonoScripts
     public class CommentSpawner : MonoBehaviour
     {
         [SerializeField] GameObject commentSection = null!; // [Prerequisite
-        [SerializeField] GameObject commentPrefab = null!;
+        [SerializeField] CommentMono commentPrefab = null!;
 
         IObjectResolver _container = null!;
         CommentSpawnPointContainer _commentSpawnPointContainer = null!;
+        CommentCluster _commentCluster = null!;
 
 
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                _container.Instantiate(commentPrefab, _commentSpawnPointContainer.SpawnPosition, Quaternion.identity,
+                var comment = _container.Instantiate(commentPrefab, _commentSpawnPointContainer.SpawnPosition, Quaternion.identity,
                     commentSection.transform);
+                _commentCluster.Add(comment);
             }
         }
 
         [Inject]
         public void Initialized(
             IObjectResolver container,
-            CommentSpawnPointContainer commentSpawnPointContainer)
+            CommentSpawnPointContainer commentSpawnPointContainer,
+            CommentCluster commentCluster)
+        
         {
             _container = container;
             _commentSpawnPointContainer = commentSpawnPointContainer;
+            _commentCluster = commentCluster;
         }
     }
 }
