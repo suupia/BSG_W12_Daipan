ea0441beecf93ab5ccb0393a57ad42997beb96ad
aya097 <akase097@gmail.com>
2024-05-22 23:15:36 +0900

    move : comment parameter files

--- Assets/Daipan/LevelDesign/Comment/Scripts/CommentParams.cs
+++ Assets/Daipan/LevelDesign/Comment/Scripts/CommentParams.cs
@@ -1,6 +1,8 @@
 using Daipan.Comment.Scripts;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Daipan.LevelDesign.Comment.Scripts
@@ -8,8 +10,53 @@ namespace Daipan.LevelDesign.Comment.Scripts
     [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Comment/Parameters", order = 1)]
     public sealed class CommentParams : ScriptableObject
     {
-        //[Header("コメントのレベルデザインはこちら！！")]
-        //[Space(30)]
+        [Header("コメントのレベルデザインはこちら！！")]
+        [Space(30)]
 
+
+        [Header("コメントのタイプ")]
+        [Tooltip("None      : 設定しないでください-_-\n" +
+                 "Normal    : ")]
+        [SerializeField] CommentType commentType = CommentType.None;
+
+        [SerializeField] Sprite sprite = null!;
+        public Sprite Sprite => sprite;
+
+        public CommentEnum GetCommentEnum
+        {
+            get
+            {
+                CommentEnumChecker.CheckEnum();
+                return CommentEnum.Values.First(x => x.Name == commentType.ToString());
+            }
+        }
+    }
+
+    
+
+    static class CommentEnumChecker
+    {
+        static bool _isCheckedEnum;
+
+        public static void CheckEnum()
+        {
+            if (_isCheckedEnum) return;
+            foreach (var type in Enum.GetValues(typeof(CommentType)).Cast<CommentType>())
+            {
+                var comment = CommentEnum.Values.FirstOrDefault(x => x.Name == type.ToString());
+                if (comment.Equals(default(CommentEnum)))
+                    Debug.LogWarning($"CommentEnum with name {type.ToString()} not found.");
+            }
+
+            _isCheckedEnum = true;
+        }
+    }
+
+    enum CommentType
+    {
+        None,
+        Normal,
+        Super,
+        Spiky,
     }
 }
