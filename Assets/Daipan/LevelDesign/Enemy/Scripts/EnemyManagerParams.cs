17bc6c2eb79fcaa85123d855b22a4d56360bf663
aya097 <akase097@gmail.com>
2024-05-23 02:53:27 +0900

    save unity scene

---
+++ Assets/Daipan/LevelDesign/Enemy/Scripts/EnemyManagerParams.cs
@@ -0,0 +1,15 @@
+using Daipan.Comment.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Daipan.LevelDesign.Enemy.Scripts
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Enemy/ManagerParameters", order = 1)]
+    public sealed class EnemyManagerParams : ScriptableObject
+    {
+        //[Header(エネミーのレベルデザインはこちら！！")]
+        //[Space(30)]
+
+    }
+}
