4bdda9540c22bb642954634197c31035ecd66108
suupia <suupia771@gmail.com>
2024-05-22 23:07:33 +0900

    Rename to GetScene

--- Assets/Daipan/Player/Tests/GetScene.cs
+++ Assets/Daipan/Player/Tests/GetScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-public class getscene:MonoBehaviour
+public class GetScene:MonoBehaviour
 {
 
     // Update is called once per frame
