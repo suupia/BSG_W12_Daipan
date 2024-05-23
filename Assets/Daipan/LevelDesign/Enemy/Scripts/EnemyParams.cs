30f37a7263da030f59a113039598491d0be71acf
aya097 <akase097@gmail.com>
2024-05-23 17:18:11 +0900

    add : EnemyParams

--- Assets/Daipan/LevelDesign/Enemy/Scripts/EnemyParams.cs
+++ Assets/Daipan/LevelDesign/Enemy/Scripts/EnemyParams.cs
@@ -1,6 +1,9 @@
 using Daipan.Comment.Scripts;
+using Daipan.Enemy.Scripts;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Daipan.LevelDesign.Enemy.Scripts
@@ -8,8 +11,67 @@ namespace Daipan.LevelDesign.Enemy.Scripts
     [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Enemy/Parameters", order = 1)]
     public sealed class EnemyParams : ScriptableObject
     {
-        //[Header(エネミーのレベルデザインはこちら！！")]
-        //[Space(30)]
+        [Header("エネミーのレベルデザインはこちら！！")]
+        [Space(30)]
 
+        [Header("エネミーのタイプ")]
+        [Tooltip("None      : 設定しないでください-_-\n" +
+                 "W         : Wキーで倒せる敵\n" +
+                 "A         : Aキーで倒せる敵\n" +
+                 "S         : Sキーで倒せる敵\n" +
+                 "Boss      ; ボス")]
+        [SerializeField] EnemyType enemyType = EnemyType.None;
+
+        [Header("エネミーのHP")]
+        public int HPAmount;
+
+        [Header("エネミーの攻撃力")] 
+        public int attackAmount;
+
+        [Header("移動速度 [unit/s]")]
+        [Min(0)]
+        public float moveSpeed_ups;
+
+        [Header("攻撃間隔")]
+        [Min(0)]
+        public float attackDelaySec;
+
+        [Header("このエネミーを倒したときの視聴者数の変化")]
+        public float diffViewer;
+
+        public EnemyEnum GetEnemyEnum
+        {
+            get
+            {
+                EnemyEnumChecker.CheckEnum();
+                return EnemyEnum.Values.First(x => x.Name == enemyType.ToString());
+            }
+        }
+
+        static class EnemyEnumChecker
+        {
+            static bool _isCheckedEnum;
+
+            public static void CheckEnum()
+            {
+                if (_isCheckedEnum) return;
+                foreach (var type in Enum.GetValues(typeof(EnemyType)).Cast<EnemyType>())
+                {
+                    var enemy = EnemyEnum.Values.FirstOrDefault(x => x.Name == type.ToString());
+                    if (enemy.Equals(default(EnemyEnum))) Debug.LogWarning($"EnemyEnum with name {type.ToString()} not found.");
+                }
+
+                _isCheckedEnum = true;
+            }
+        }
+
+        enum EnemyType
+        {
+            None,
+            W,
+            A,
+            S,
+            Boss
+        }
     }
 }
