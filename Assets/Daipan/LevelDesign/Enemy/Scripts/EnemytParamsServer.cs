3831cccfe81d111d237e37c990a831e8172edeab
suupia <suupia771@gmail.com>
2024-05-26 13:55:50 +0900

    Dived parameters in EnemyParam

--- Assets/Daipan/LevelDesign/Enemy/Scripts/EnemytParamsServer.cs
+++ Assets/Daipan/LevelDesign/Enemy/Scripts/EnemytParamsServer.cs
@@ -29,14 +29,14 @@ namespace Daipan.LevelDesign.Enemy.Scripts
         
         void CheckIsValid(EnemyParamsManager parameters)
         {
-            foreach (var enemyLifeParam in parameters.enemyLifeParams)
+            Debug.Log($"EnemeyCount : {parameters.enemyParams.Count}");
+            foreach (var enemyParam in parameters.enemyParams)
             {
-                var enemyParam = enemyLifeParam.enemyParams;
-                if (enemyParam.attackAmount <= 0)
+                if (enemyParam.EnemyAttackParam.attackAmount <= 0)
                 {
                     Debug.LogWarning($"{enemyParam.GetEnemyEnum}の攻撃力が0以下です。");
                 }
-                if (enemyParam.attackRange <= 0)
+                if (enemyParam.EnemyAttackParam.attackRange <= 0)
                 {
                     Debug.LogWarning($"{enemyParam.GetEnemyEnum}の攻撃範囲が0以下です。");
                 }
@@ -48,22 +48,22 @@ namespace Daipan.LevelDesign.Enemy.Scripts
 
         public float GetSpeed(EnemyEnum enemyEnum)
         {
-            return GetEnemyParams(enemyEnum).moveSpeedPerSec;
+            return GetEnemyParams(enemyEnum).EnemyMoveParam.moveSpeedPerSec;
         }
 
         public EnemyAttackParameter GetAttackParameter(EnemyEnum enemyEnum)
         {
             var enemy = GetEnemyParams(enemyEnum);
             return new EnemyAttackParameter(
-                enemy.attackAmount,
-                enemy.attackDelaySec,
-                enemy.attackRange
+                enemy.EnemyAttackParam.attackAmount,
+                enemy.EnemyAttackParam.attackDelaySec,
+                enemy.EnemyAttackParam.attackRange
             );
         }
 
         public int GetHp(EnemyEnum enemyEnum)
         {
-            return GetEnemyParams(enemyEnum).hpAmount; 
+            return GetEnemyParams(enemyEnum).EnemyHpParam.hpAmount; 
         }
 
         public Sprite GetSprite(EnemyEnum enemyEnum)
@@ -92,17 +92,17 @@ namespace Daipan.LevelDesign.Enemy.Scripts
             // 通常敵のType決め
             List<float> ratio = new();
 
-            foreach (var enemyLife in _enemyParamsManager.enemyLifeParams)
+            foreach (var enemyLife in _enemyParamsManager.enemyParams)
             {
-                ratio.Add(enemyLife.spawnRatio);
+                ratio.Add(enemyLife.EnemySpawnParam.spawnRatio);
             }
 
-            return _enemyParamsManager.enemyLifeParams[Randoms.RandomByRatio(ratio)].enemyParams.GetEnemyEnum;
+            return _enemyParamsManager.enemyParams[Randoms.RandomByRatio(ratio)].GetEnemyEnum;
         }
 
-        EnemyParams GetEnemyParams(EnemyEnum enemyEnum)
+        EnemyParam GetEnemyParams(EnemyEnum enemyEnum)
         {
-            return _enemyParamsManager.enemyLifeParams.First(c => c.enemyParams.GetEnemyEnum == enemyEnum).enemyParams;
+            return _enemyParamsManager.enemyParams.First(c => c.GetEnemyEnum == enemyEnum);
         }
 
         #endregion
