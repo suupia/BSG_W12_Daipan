ab9f208db1241aebac30a490c6f1d317dc17cae1
aya097 <akase097@gmail.com>
2024-05-31 00:07:09 +0900

    add : TowerPrefabLoader

---
+++ Assets/Daipan/Tower/Scripts/TowerPrefabLoader.cs
@@ -0,0 +1,22 @@
+#nullable enable
+using Daipan.Comment.MonoScripts;
+using Daipan.Stream.Scripts.Utility;
+using Daipan.Stream.Scripts.Utility.Scripts;
+
+namespace Daipan.Tower.Scripts
+{
+    public sealed class TowerPrefabLoader : IPrefabLoader<TowerMono>
+    {
+        readonly PrefabLoaderFromResources<TowerMono> _loader;
+
+        public TowerPrefabLoader()
+        {
+            _loader = new PrefabLoaderFromResources<TowerMono>("Tower");
+        }
+
+        public TowerMono Load()
+        {
+            return _loader.Load();
+        }
+    }
+}
