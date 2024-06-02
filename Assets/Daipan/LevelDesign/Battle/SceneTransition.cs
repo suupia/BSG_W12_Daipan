c9a10445a9d558185ef099cea6d8a507dfb92ac4
TK0925 <0925tkg@gmail.com>
2024-06-02 10:22:57 +0900

    add:SceneTransition

--- Assets/Daipan/LevelDesign/Battle/SceneTransition.cs
+++ Assets/Daipan/LevelDesign/Battle/SceneTransition.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class SceneTransition:MonoBehaviour
+namespace SceneName
 {
-    Button button;
-    [SerializeField]
-    private string SceneName;
-
-    private void Start()
+    public static class SceneTransition
     {
-        button = GetComponent<Button>();
-        button.onClick.AddListener(() => SceneManager.LoadScene(SceneName));
+        static readonly Dictionary<SceneName, string> SeneNameTable = new()
+        {
+
+        {SceneName.TitleScene,"TitleScene" },
+        {SceneName.DaipanScene,"DaipanScene" },
+        {SceneName.ResultScene,"ResultScene" },
+
+        };
+
+        public static void TransitioningScene(SceneName nextScene)
+        {
+            if (SeneNameTable.TryGetValue(nextScene, out var sceneName))
+            {
+                Debug.Log($"Transitioning to {sceneName}");
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+            }
+            else
+            {
+                Debug.LogError($"{nextScene} is not registered in sceneNameTable.");
+            }
+        }
+    }
+
+    public enum SceneName
+    {
+        TitleScene,
+        DaipanScene,
+        ResultScene
     }
 }
