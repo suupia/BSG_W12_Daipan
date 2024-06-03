b387a5e692bd83bac095b4c5a7f001d3f223b933
suupia <suupia771@gmail.com>
2024-06-04 00:48:39 +0900

    Fix EnumEnumerationChecker.cs

--- Assets/Daipan/Utility/Scripts/EnumEnumerationChecker.cs
+++ Assets/Daipan/Utility/Scripts/EnumEnumerationChecker.cs
@@ -1,5 +1,7 @@
 #nullable enable
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Daipan.Enemy.Scripts;
 using UnityEngine;
@@ -8,13 +10,13 @@ namespace Daipan.Utility.Scripts
 {
     static class EnumEnumerationChecker
     {
-        static bool _isCheckedEnum;
-
+        static readonly HashSet<Type> CheckedEnums = new(); 
         public static void CheckEnum<TEnum,TEnumeration>() 
             where TEnum : Enum
             where TEnumeration : Enumeration
         {
-            if (_isCheckedEnum) return;
+            if (CheckedEnums.Contains(typeof(TEnum)))
+                return;
             foreach (var type in Enum.GetValues(typeof(TEnum)).Cast<TEnum>())
             {
                 var enumeration = Enumeration.GetAll<TEnumeration>().FirstOrDefault(x => x.Name == type.ToString());
@@ -27,7 +29,7 @@ namespace Daipan.Utility.Scripts
                     Debug.LogWarning($"EnemyEnum with name {type.ToString()} not found.");
             }
 
-            _isCheckedEnum = true;
+            CheckedEnums.Add(typeof(TEnum)); 
         }
     }
 }
