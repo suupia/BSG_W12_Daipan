7cc8525c2ab7776b4e6495c1655b51db7792bde1
aya097 <akase097@gmail.com>
2024-06-07 18:09:29 +0900

    add : set SerialInput

--- Assets/Daipan/Input/Scripts/SerialInput.cs
+++ Assets/Daipan/Input/Scripts/SerialInput.cs
@@ -1,69 +1,89 @@
-//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.IO.Ports;
-//using System.Threading;
-//using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using UnityEngine;
 
-//namespace Daipan.Input.Scripts
-//{
-//    public class SerialInput : MonoBehaviour
-//    {
+namespace Daipan.InputSerial.Scripts
+{
+    public class SerialInput : MonoBehaviour
+    {
 
-//        [SerializeField] private string portName = "COM?";
-//        [SerializeField] private int baurate = 115200;
+        [SerializeField] private string portName = "COM?";
+        [SerializeField] private int baurate = 115200;
 
-//        private SerialPort serial;
-//        private bool isLoop = true;
-//        private Thread thread;
+        private SerialPort serial;
+        private bool isLoop = true;
+        private Thread thread;
 
 
-//        public int number = 0;
-//        void Start()
-//        {
-//            this.serial = new SerialPort(portName, baurate, Parity.None, 8, StopBits.One);
-//            serial.DtrEnable = true;
-//            serial.RtsEnable = true;
-//            try
-//            {
-//                this.serial.Open();
+        public int number = 0;
+        void Start()
+        {
+            this.serial = new SerialPort(portName, baurate, Parity.None, 8, StopBits.One);
+            serial.DtrEnable = true;
+            serial.RtsEnable = true;
 
-//            }
-//            catch (Exception e)
-//            {
-//                Debug.Log("ポートが開けませんでした。設定している値が間違っている場合があります" +
-//                          e.Message);
-//            }
+            //テスト
 
-//            try
-//            {
-//                thread = new Thread(ReadData);
-//                thread.Start();
-//                //別スレッドで実行  
-//                //Scheduler.ThreadPool.Schedule(() => ReadData()).AddTo(this);
-//            }
-//            catch
-//            {
-//                Debug.Log("なんかミスってる");
-//            }
-//        }
+            for(int portNum = 0; portNum < 11; portNum++)
+            {
+                serial = new SerialPort(portName + portNum, baurate, Parity.None, 8, StopBits.One);
+                serial.DtrEnable = true;
+                serial.RtsEnable = true;
+                try
+                {
+                    serial.Open();
+                }
+                catch(Exception e)
+                {
+                    Debug.LogWarning(portName + portNum + "ポートが開けませんでした。設定している値が間違っている場合があります" +
+                         e.Message);
+                    continue;
+                }
+                portNum = 1000;
+            }
+            //try
+            //{
+            //    this.serial.Open();
 
-//        //データ受信時に呼ばれる
-//        public void ReadData()
-//        {
-//            while (this.isLoop)
-//            {
-//                //ReadLineで読み込む
-//                string message = this.serial.ReadLine();
-//                Debug.Log(message);
-//                number = int.Parse(message);
-//            }
-//        }
+            //}
+            //catch (Exception e)
+            //{
+            //    Debug.LogWarning("ポートが開けませんでした。設定している値が間違っている場合があります" +
+            //              e.Message);
+            //}
 
-//        void OnDestroy()
-//        {
-//            this.isLoop = false;
-//            this.serial.Close();
-//        }
-//    }
-//}
+            try
+            {
+                thread = new Thread(ReadData);
+                thread.Start();
+                //別スレッドで実行  
+                //Scheduler.ThreadPool.Schedule(() => ReadData()).AddTo(this);
+            }
+            catch
+            {
+                Debug.Log("なんかミスってる");
+            }
+        }
+
+        //データ受信時に呼ばれる
+        public void ReadData()
+        {
+            while (this.isLoop)
+            {
+                //ReadLineで読み込む
+                string message = this.serial.ReadLine();
+                Debug.Log(message);
+                number = int.Parse(message);
+            }
+        }
+
+        void OnDestroy()
+        {
+            this.isLoop = false;
+            this.serial.Close();
+        }
+    }
+}
