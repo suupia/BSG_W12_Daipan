de56dd086509d7aa61b2b4aa1974247cb3a60a76
suupia <suupia771@gmail.com>
2024-06-08 15:45:37 +0900

    Add Update() logic to PlayerAttackEffectMono

--- Assets/Daipan/Player/MonoScripts/PlayerAttackEffectMono.cs
+++ Assets/Daipan/Player/MonoScripts/PlayerAttackEffectMono.cs
@@ -1,9 +1,24 @@
 #nullable enable
+using System;
 using UnityEngine;
 
 namespace Daipan.Player.MonoScripts
 {
     public class PlayerAttackEffectMono : MonoBehaviour
     {
+        public Func<Vector3>? TargetPosition;
+        double _speed = 2;
+            
+        void Update()
+        {
+            if (TargetPosition == null)
+            {
+                Debug.LogWarning("TargetPosition is null");
+                return;
+            }
+            
+            var direction = (TargetPosition() - transform.position).normalized;
+            transform.position += (Vector3)direction * (float)(_speed * Time.deltaTime);
+        }
     }
 }
