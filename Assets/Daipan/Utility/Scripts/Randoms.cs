46337d7d6c6bc26f993a75bebbfbdc69f3f7464b
suupia <suupia771@gmail.com>
2024-06-15 09:29:52 +0900

    Fix RandomByRatios and tests

--- Assets/Daipan/Utility/Scripts/Randoms.cs
+++ Assets/Daipan/Utility/Scripts/Randoms.cs
@@ -2,6 +2,7 @@ using Daipan.LevelDesign.Enemy.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Daipan.Extensions;
 using UnityEngine;
 
 namespace Daipan.Utility.Scripts
@@ -21,25 +22,27 @@ namespace Daipan.Utility.Scripts
                 Debug.LogWarning("rand must be between 0 and 1.");
                 return -1;
             }
+
             if (ratios.Sum() <= 0)
             {
                 Debug.LogWarning("Sum of ratios must be greater than 0.");
                 return -1;
             }
 
-            var accumulatedRatios = ratios.Aggregate(new List<double>(), (xs, x) 
-                => xs.Concat(new[] { xs.FirstOrDefault() + x }).ToList());
-            var normalizedAccumulatedRatios = accumulatedRatios.Select(ratio => ratio / accumulatedRatios.Last()).ToList();
+            var accumulatedRatios = ratios.Scan(0.0, (acc, ratio) => acc + ratio).ToList();
+            var normalizedAccumulatedRatios =
+                accumulatedRatios.Select(ratio => ratio / accumulatedRatios.Last()).ToList();
             var result = normalizedAccumulatedRatios.Select((ratio, index) => new { ratio, index })
                 .FirstOrDefault(pair => random < pair.ratio);
-            
-            if(result == null)
+            // Debug.Log($"normalizedAccumulatedRatios: {string.Join(",", normalizedAccumulatedRatios)}, random: {random}, result: {result}");
+            if (result == null)
             {
-                Debug.LogWarning($"Failed to get random index. normalizedAccumulatedRatios: {string.Join(",",normalizedAccumulatedRatios)}, random: {random}");
+                Debug.LogWarning(
+                    $"Failed to get random index. normalizedAccumulatedRatios: {string.Join(",", normalizedAccumulatedRatios)}, random: {random}");
                 return -1;
             }
 
-            return result.index;
+            return result.index - 1;
         }
     }
 }
--- Assets/Daipan/Utility/Tests/RandomByRatiosTest.cs
+++ Assets/Daipan/Utility/Tests/RandomByRatiosTest.cs
@@ -14,31 +14,54 @@ namespace Daipan.Utility.Tests
         [Test]
         public void AccumulateRatiosTest1()
         {
-            var ratios = new [] { 1.0, 2.0, 3.0 };
-           
-            var accumulatedRatios = ratios.Scan(0.0, (acc, ratio ) => acc + ratio).ToArray();
-            Debug.Log($"accumulatedRatios: {string.Join(",",accumulatedRatios)}");
-            Assert.AreEqual( new [] { 0.0, 1.0, 3.0, 6.0 }, accumulatedRatios);
+            var ratios = new[] { 1.0, 2.0, 3.0 };
+
+            var accumulatedRatios = ratios.Scan(0.0, (acc, ratio) => acc + ratio).ToArray();
+            Debug.Log($"accumulatedRatios: {string.Join(",", accumulatedRatios)}");
+            Assert.AreEqual(new[] { 0.0, 1.0, 3.0, 6.0 }, accumulatedRatios);
         }
 
         [Test]
         public void RandomByRatiosTest1()
         {
             // Arrange
-            var ratios = new List<double> { 1.0, 2.0, 3.0 };
+            var ratios = new List<double> { 0.2, 0.2, 0.6 };
 
-            var index = Randoms.RandomByRatios(ratios, 0.0);
-            Assert.AreEqual(0, index);
+            // Act and Assert
+            var index0 = Randoms.RandomByRatios(ratios, 0.0);
+            Assert.AreEqual(0, index0);
 
-            var index2 = Randoms.RandomByRatios(ratios, 0.5);
+            var index1 = Randoms.RandomByRatios(ratios, 0.1);
+            Assert.AreEqual(0, index1);
+
+            var index2 = Randoms.RandomByRatios(ratios, 0.2);
             Assert.AreEqual(1, index2);
 
-            var index3 = Randoms.RandomByRatios(ratios, 0.9);
-            Assert.AreEqual(2, index3);
+            var index3 = Randoms.RandomByRatios(ratios, 0.3);
+            Assert.AreEqual(1, index3);
 
-            var index4 = Randoms.RandomByRatios(ratios, 1.0);
-            Assert.AreEqual(-1, index4);
+            var index4 = Randoms.RandomByRatios(ratios, 0.4);
+            Assert.AreEqual(2, index4);
+
+            var index5 = Randoms.RandomByRatios(ratios, 0.5);
+            Assert.AreEqual(2, index5);
+
+            var index6 = Randoms.RandomByRatios(ratios, 0.6);
+            Assert.AreEqual(2, index6);
+
+            var index7 = Randoms.RandomByRatios(ratios, 0.7);
+            Assert.AreEqual(2, index7);
+
+            var index8 = Randoms.RandomByRatios(ratios, 0.8);
+            Assert.AreEqual(2, index8);
+
+            var index9 = Randoms.RandomByRatios(ratios, 0.99);
+            Assert.AreEqual(2, index9);
+
+            var index10 = Randoms.RandomByRatios(ratios, 1.0);
+            Assert.AreEqual(-1, index10);
         }
+
 
     }
 }
