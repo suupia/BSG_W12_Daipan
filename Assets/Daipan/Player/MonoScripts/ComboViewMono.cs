864f7bfcc2c0bc801d3870e6e2d8d2f4e699b0dd
suupia <suupia771@gmail.com>
2024-06-20 22:26:59 +0900

    Add comment to ComboViewMOno.cs

--- Assets/Daipan/Player/MonoScripts/ComboViewMono.cs
+++ Assets/Daipan/Player/MonoScripts/ComboViewMono.cs
@@ -15,11 +15,15 @@ namespace Daipan.Player.MonoScripts
         public void Initialize(ComboCounter comboCounter)
         {
             Observable.EveryValueChanged(comboCounter, x => x.ComboCount)
-                .Subscribe(_ => UpdateComboText(comboCounter.ComboCount));
+                .Subscribe(_ => UpdateComboText(comboCounter.ComboCount))
+                .AddTo(this);
         }
 
         void UpdateComboText(int comboCount)
         {
+            // comboCountが0ならフェードアウト
+            // 増える時にアニメーション
+            
             comboText.text = $"{comboCount}";
         }
     }
