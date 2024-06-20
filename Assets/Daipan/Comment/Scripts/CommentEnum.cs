00643a55f1208a45a5162fc67dbba19b1c90da0a
suupia <suupia771@gmail.com>
2024-06-21 03:10:28 +0900

    Remove enumeration class of CommentEnum

--- Assets/Daipan/Comment/Scripts/CommentEnum.cs
+++ Assets/Daipan/Comment/Scripts/CommentEnum.cs
@@ -3,29 +3,11 @@ using Daipan.Utility.Scripts;
 
 namespace Daipan.Comment.Scripts
 {
-    public class CommentEnum : Enumeration
+    public enum CommentEnum
     {
-        public static CommentEnum[] Values { get; }
-
-        CommentEnum(int id, string name) : base(id, name)
-        {
-        }
-
-        static CommentEnum()
-        {
-            Values = new[]
-            {
-                None,
-                Normal,
-                Super,
-                Spiky,
-            };
-        }
-
-        public static CommentEnum None = new(0, "None");
-        public static CommentEnum Normal = new(1, "Normal");
-        public static CommentEnum Super = new(2, "Super");
-        public static CommentEnum Spiky = new(3, "Spiky");
-        
+        None,
+        Normal,
+        Super,
+        Spiky,
     }
 }
