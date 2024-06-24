505b8a51845a4bb59241f3ab54e3e2b2da90451a
suupia <suupia771@gmail.com>
2024-06-24 17:53:20 +0900

    Remove Debug.Log()

--- Assets/Daipan/Stream/Scripts/IrritatedValue.cs
+++ Assets/Daipan/Stream/Scripts/IrritatedValue.cs
@@ -43,8 +43,6 @@ namespace Daipan.Stream.Scripts
 
             IncreasedValue += amount;
             if (Value >= MaxValue) IncreasedValue = MaxValue;
-
-            Debug.Log($"IncreaseValue(amount: {amount}) IrritatedValue : {Value}");
         }
 
         public void DecreaseValue(float amount)
