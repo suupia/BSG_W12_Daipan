c505ccdd69ae18a3073e13781110deab942a02c4
aya097 <akase097@gmail.com>
2024-06-25 02:15:29 +0900

    add : Test

---
+++ Assets/Daipan/Player/PlayerTests/PlayerAttackedCounterTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using Daipan.Player.Scripts;
+
+public class PlayerAttackedCounterTest
+{
+    [Test]
+    public void PlayerAttackedCounterTestWith10()
+    {
+        // Arrange
+        var counter = new PlayerAttackedCounter(10);
+
+        // Act
+        for(int i = 0; i < 10; i++)
+        {
+            counter.CountUp();
+        }
+
+        // Assert
+        Assert.IsTrue(counter.isOverThreshold);
+
+        // Act
+        counter.CountUp();
+
+        // Assert
+        Assert.IsFalse(counter.isOverThreshold);
+    }
+}
