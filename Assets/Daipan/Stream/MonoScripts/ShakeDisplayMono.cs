66637d68ea367c5f89d2b330ae7f58d199df8087
aya097 <akase097@gmail.com>
2024-06-29 02:23:37 +0900

    use ShakeDisplayMono in DaipanExecutor

--- Assets/Daipan/Stream/MonoScripts/ShakeDisplayMono.cs
+++ Assets/Daipan/Stream/MonoScripts/ShakeDisplayMono.cs
@@ -11,10 +11,25 @@ namespace Daipan.Stream.MonoScripts
         [SerializeField] float shakePower;
         [SerializeField] int ShakeNum;
 
+        Vector3 _originalPosition;
+
+        private void Start()
+        {
+            _originalPosition = Camera.main.transform.position;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace)) Daipan();
+        }
 
         public void Daipan()
         {
-            Camera.main.transform.DOShakePosition(duration, shakePower, ShakeNum, 1, false, true);
+            Camera.main.transform.DOShakePosition(duration, shakePower, ShakeNum, 1, false, true)
+                .OnComplete(() =>
+                {
+                    transform.position = _originalPosition;
+                });
         }
     }
 }
