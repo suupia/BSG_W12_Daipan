003c7372dc3cd248831090ac8be7f25c25495b20
suupia <suupia771@gmail.com>
2024-06-28 16:36:47 +0900

    Add EnemyViewParamRed to test enemy animations

--- Assets/Daipan/Enemy/Tests/EnemyHighlight/EnemyHighlightTestInputMono.cs
+++ Assets/Daipan/Enemy/Tests/EnemyHighlight/EnemyHighlightTestInputMono.cs
@@ -1,6 +1,7 @@
 #nullable enable
 using System.Collections;
 using System.Collections.Generic;
+using Daipan.Enemy.Interfaces;
 using Daipan.Enemy.MonoScripts;
 using Daipan.Enemy.Scripts;
 using UnityEngine;
@@ -11,6 +12,15 @@ public class EnemyHighlightTestInputMono : MonoBehaviour
 
     void Start()
     {
-       // enemyViewMono.SetView(EnemyEnum.Red);
+       enemyViewMono.SetDomain( new EnemyViewParamRed());
+    }
+
+    class EnemyViewParamRed : IEnemyViewParamData
+    {
+        public EnemyEnum GetEnemyEnum() => EnemyEnum.Red;
+        public Color GetBodyColor() => Color.red;
+        public Color GetEyeColor() => new(226f / 255f, 248f / 255f, 227f / 255f);
+        public Color GetEyeBallColor() => Color.red;
+        public Color GetLineColor() =>new(111f / 255f, 87f / 255f, 107f / 255f);
     }
 }
