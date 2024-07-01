61a0da8f36b705c2bdd7fecf9bc03e77c85170dd
suupia <suupia771@gmail.com>
2024-07-01 12:40:03 +0900

    DigitSplitViewMono.cs

---
+++ Assets/Daipan/Stream/MonoScripts/DigitSplitViewMono.cs
@@ -0,0 +1,32 @@
+#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace Daipan.Stream.MonoScripts
+{
+    public class DigitSplitViewMono : MonoBehaviour
+    {
+        [SerializeField] List<TextMeshProUGUI> digitTexts = new();
+
+        public void SetDigit(int digit)
+        {
+            var digitString = digit.ToString().Reverse().ToArray();
+
+            if (digitTexts.Count < digitString.Length)
+            {
+                Debug.LogWarning(
+                    $"digitTexts.Count < digitString.Length. digitTexts.Count: {digitTexts.Count}, digitString.Length: {digitString.Length}");
+                return;
+            }
+
+            for (var i = 0; i < digitTexts.Count; i++)
+                if (i < digitString.Length)
+                    digitTexts[i].text = digitString[i].ToString();
+                else
+                    digitTexts[i].text = "";
+        }
+    }
+}
