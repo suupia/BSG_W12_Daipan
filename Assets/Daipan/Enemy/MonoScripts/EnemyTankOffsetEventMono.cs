6fbbf05c5d5ee7a80fa3cef5a27b42372b987c37
suupia <suupia771@gmail.com>
2024-07-04 01:01:32 +0900

    Fix FillMin in EnemyTankOffsetEventMono

--- Assets/Daipan/Enemy/MonoScripts/EnemyTankOffsetEventMono.cs
+++ Assets/Daipan/Enemy/MonoScripts/EnemyTankOffsetEventMono.cs
@@ -9,72 +9,80 @@ namespace Daipan.Enemy.MonoScripts
     {
         [SerializeField] double[] offsetRatioMove = { 0, 0, 0, 0, 0, 0, 0, 0 };
         [SerializeField] double[] offsetRatioAttack = { 0, 0, 0, 0, 0, 0, 0, 0 };
-        
-        [Header("Attackの時の傾ける角度（弧度法）")]
-        [SerializeField] float attackAngle = 0;
-        
+
+        [Header("Attackの時の傾ける角度（弧度法）")] [SerializeField]
+        float attackAngle = 0;
+
         Material _tankGaugeMaterial = null!;
         [SerializeField] Animator animator = null!;
-           
-        // Tankの画像が全体の画像のサイズに合わせられているため、0.47でマックスになることに注意
+
+        // Tankの画像が全体の画像のサイズに合わせられているため、FillMin, FillMaxで調整
+        // FinMinで0を、FillMaxで1を取るようにする
+        const double FillMin = 0.1;
         const double FillMax = 0.6;
-        
-        public double Ratio { get;  set; }
-        
-        public double CurrentOffsetRatioMove { get; private set; } 
+
+        public double Ratio { get; set; }
+
+        public double CurrentOffsetRatioMove { get; private set; }
         public double CurrentOffsetRatioAttack { get; private set; }
-        
+
         void Awake()
         {
-            _tankGaugeMaterial = animator.GetComponent<SpriteRenderer>().material; 
+            _tankGaugeMaterial = animator.GetComponent<SpriteRenderer>().material;
         }
-        
+
         void Frame0()
         {
-            CurrentOffsetRatioMove = offsetRatioMove[0]; 
+            CurrentOffsetRatioMove = offsetRatioMove[0];
             CurrentOffsetRatioAttack = offsetRatioAttack[0];
             Update();
         }
 
         void Frame1()
         {
-            CurrentOffsetRatioMove = offsetRatioMove[1]; 
+            CurrentOffsetRatioMove = offsetRatioMove[1];
             CurrentOffsetRatioAttack = offsetRatioAttack[1];
             Update();
-        } 
+        }
+
         void Frame2()
         {
             CurrentOffsetRatioMove = offsetRatioMove[2];
             CurrentOffsetRatioAttack = offsetRatioAttack[2];
             Update();
         }
+
         void Frame3()
         {
-            CurrentOffsetRatioMove = offsetRatioMove[3]; 
+            CurrentOffsetRatioMove = offsetRatioMove[3];
             CurrentOffsetRatioAttack = offsetRatioAttack[3];
             Update();
         }
+
         void Frame4()
         {
-            CurrentOffsetRatioMove = offsetRatioMove[4]; 
+            CurrentOffsetRatioMove = offsetRatioMove[4];
             CurrentOffsetRatioAttack = offsetRatioAttack[4];
             Update();
         }
+
         void Frame5()
         {
-            CurrentOffsetRatioMove = offsetRatioMove[5]; 
+            CurrentOffsetRatioMove = offsetRatioMove[5];
             CurrentOffsetRatioAttack = offsetRatioAttack[5];
             Update();
         }
+
         void Frame6()
         {
-            CurrentOffsetRatioMove = offsetRatioMove[6]; 
+            CurrentOffsetRatioMove = offsetRatioMove[6];
             CurrentOffsetRatioAttack = offsetRatioAttack[6];
             Update();
         }
+
         void Frame7()
         {
-            CurrentOffsetRatioMove = offsetRatioMove[7]; 
+            CurrentOffsetRatioMove = offsetRatioMove[7];
             CurrentOffsetRatioAttack = offsetRatioAttack[7];
             Update();
         }
@@ -82,20 +90,20 @@ namespace Daipan.Enemy.MonoScripts
         void Update()
         {
             var clipName = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
-            Debug.Log($"clipName:{clipName}");
-            
+
             // あまりよくないが、clipNameで分岐
             if (clipName == "Boss1_AttackMotion_Tank")
             {
-                _tankGaugeMaterial.SetFloat("_Ratio", (float)((Ratio + CurrentOffsetRatioAttack) * FillMax));
+                _tankGaugeMaterial.SetFloat("_Ratio",
+                    (float)((Ratio + CurrentOffsetRatioAttack) * (FillMax - FillMin) + FillMin));
                 _tankGaugeMaterial.SetFloat("_RotationAngle", attackAngle);
             }
             else
             {
-                _tankGaugeMaterial.SetFloat("_Ratio", (float)((Ratio + CurrentOffsetRatioMove) * FillMax));
+                _tankGaugeMaterial.SetFloat("_Ratio",
+                    (float)((Ratio + CurrentOffsetRatioMove) * (FillMax - FillMin) + FillMin));
                 _tankGaugeMaterial.SetFloat("_RotationAngle", 0);
             }
         }
     }
 }
-
