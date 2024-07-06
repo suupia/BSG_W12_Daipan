4b7803feaaa3c058cd5b719a294f3397b59e68d2
suupia <suupia771@gmail.com>
2024-07-06 16:09:48 +0900

    Change static function in DaipanScope to public

--- Assets/Daipan/Daipan/DaipanScope.cs
+++ Assets/Daipan/Daipan/DaipanScope.cs
@@ -23,7 +23,6 @@ using Daipan.Player.Scripts;
 using Daipan.Stream.MonoScripts;
 using Daipan.Stream.Scripts;
 using Daipan.Stream.Scripts.Utility;
-using Daipan.Stream.Tests;
 using Daipan.Streamer.Scripts;
 using Daipan.Tower.MonoScripts;
 using Daipan.Tower.Scripts;
@@ -52,150 +51,142 @@ namespace Daipan.Daipan
         [SerializeField] TowerParams towerParams = null!;
         [SerializeField] ComboParamManager comboParamManager = null!;
 
-        protected override void Configure(IContainerBuilder builder)
+        public static void RegisterStream(IContainerBuilder builder, StreamParam streamParam)
         {
+            // Parameters 
+            builder.RegisterInstance(new StreamParamDataBuilder(builder, streamParam));
+            builder.RegisterInstance(streamParam.viewer);
+            builder.RegisterInstance(streamParam.daipan);
             // Stream
-            RegisterStream(builder, streamParam);
+            builder.Register<StreamPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<StreamMono>>();
+            builder.Register<ViewerNumber>(Lifetime.Scoped);
+            builder.Register<StreamStatus>(Lifetime.Scoped);
+            builder.Register<IStart, StreamSpawner>(Lifetime.Scoped).AsSelf();
+            builder.Register<StreamTimer>(Lifetime.Scoped).AsSelf().As<IStart>().As<IUpdate>();
+        }
 
-            static void RegisterStream(IContainerBuilder builder, StreamParam streamParam)
+        public static void RegisterComment(IContainerBuilder builder, CommentParamManager commentParamManager)
+        {
+            // Parameters 
+            builder.RegisterInstance(commentParamManager);
+            builder.Register<CommentParamsServer>(Lifetime.Scoped);
+            builder.RegisterComponentInHierarchy<CommentPosition>();
+            // Comment 
+            builder.Register<CommentPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<CommentMono>>();
+            builder.Register<AntiCommentPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<AntiCommentMono>>();
+            builder.Register<CommentCluster>(Lifetime.Scoped);
+            builder.Register<IUpdate, CommentSpawner>(Lifetime.Scoped).AsSelf();
+            builder.Register<AntiCommentCluster>(Lifetime.Scoped);
+            builder.Register<IUpdate, AntiCommentRelocate>(Lifetime.Scoped);
+        }
+
+        public static void RegisterPlayer(IContainerBuilder builder, PlayerParamManager playerParamManager)
+        {
+            // Parameters
+            PlayerPositionMono SetUpPlayerPositionMono()
             {
-                // Parameters 
-                builder.RegisterInstance(new StreamParamDataBuilder(builder, streamParam));
-                builder.RegisterInstance(streamParam.viewer);
-                builder.RegisterInstance(streamParam.daipan);
-                // Stream
-                builder.Register<StreamPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<StreamMono>>();
-                builder.Register<ViewerNumber>(Lifetime.Scoped);
-                builder.Register<StreamStatus>(Lifetime.Scoped);
-                builder.Register<IStart, StreamSpawner>(Lifetime.Scoped).AsSelf();
+                var lanePositionMono = FindObjectOfType<LanePositionMono>();
+                var playerPositionMono =
+                    new GameObject($"PlayerPositionMono(Runtime)").AddComponent<PlayerPositionMono>();
+                playerPositionMono.playerSpawnedPoint = lanePositionMono.playerSpawnedPosition;
+                playerPositionMono.playerAttackEffectPosition = new PlayerAttackEffectPosition();
+                playerPositionMono.playerAttackEffectPosition.attackEffectSpawnedPoint =
+                    lanePositionMono.attackEffectSpawnedPoint;
+                playerPositionMono.playerAttackEffectPosition.attackEffectDespawnedPoint =
+                    lanePositionMono.attackEffectDespawnedPoint;
+                return playerPositionMono;
             }
 
-            // Comment
-            RegisterComment(builder, commentParamManager);
-
-            static void RegisterComment(IContainerBuilder builder, CommentParamManager commentParamManager)
-            {
-                // Parameters 
-                builder.RegisterInstance(commentParamManager);
-                builder.Register<CommentParamsServer>(Lifetime.Scoped);
-                builder.RegisterComponentInHierarchy<CommentPosition>();
-                // Comment 
-                builder.Register<CommentPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<CommentMono>>();
-                builder.Register<AntiCommentPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<AntiCommentMono>>();
-                builder.Register<CommentCluster>(Lifetime.Scoped);
-                builder.Register<IUpdate, CommentSpawner>(Lifetime.Scoped).AsSelf();
-                builder.Register<AntiCommentCluster>(Lifetime.Scoped);
-                builder.Register<IUpdate, AntiCommentRelocate>(Lifetime.Scoped);
-            }
-
-            builder.Register<DaipanExecutor>(Lifetime.Scoped);
-
+            builder.RegisterInstance(new PlayerPositionMonoBuilder(builder, SetUpPlayerPositionMono()));
+            builder.RegisterInstance(playerParamManager);
+            builder.Register<PlayerParamDataContainer>(Lifetime.Scoped).As<IPlayerParamDataContainer>();
+            builder.Register<PlayerHpParamData>(Lifetime.Scoped).As<IPlayerHpParamData>();
+            builder.Register<PlayerAntiCommentParamData>(Lifetime.Scoped).As<IPlayerAntiCommentParamData>();
             // Player
-            RegisterPlayer(builder, playerParamManager);
+            builder.Register<PlayerPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<PlayerMono>>();
+            builder.Register<PlayerHolder>(Lifetime.Scoped);
+            builder.Register<PlayerAttackedCounter>(Lifetime.Scoped);
+            builder.Register<DaipanExecutor>(Lifetime.Scoped);
+            builder.Register<IStart, PlayerSpawner>(Lifetime.Scoped);
+            // Attack
+            builder.Register<PlayerAttackEffectPrefabLoader>(Lifetime.Scoped)
+                .As<IPrefabLoader<PlayerAttackEffectMono>>();
+            builder.Register<PlayerAttackEffectSpawner>(Lifetime.Scoped);
+            builder.Register<PlayerAttackEffectBuilder>(Lifetime.Scoped);
+            builder.Register<AttackExecutor>(Lifetime.Transient).As<IAttackExecutor>();
+        }
 
-            static void RegisterPlayer(IContainerBuilder builder, PlayerParamManager playerParamManager)
-            {
-                // Parameters
-                PlayerPositionMono SetUpPlayerPositionMono()
-                {
-                    var lanePositionMono = FindObjectOfType<LanePositionMono>();
-                    var playerPositionMono =
-                        new GameObject($"PlayerPositionMono(Runtime)").AddComponent<PlayerPositionMono>();
-                    playerPositionMono.playerSpawnedPoint = lanePositionMono.playerSpawnedPosition;
-                    playerPositionMono.playerAttackEffectPosition = new PlayerAttackEffectPosition();
-                    playerPositionMono.playerAttackEffectPosition.attackEffectSpawnedPoint =
-                        lanePositionMono.attackEffectSpawnedPoint;
-                    playerPositionMono.playerAttackEffectPosition.attackEffectDespawnedPoint =
-                        lanePositionMono.attackEffectDespawnedPoint;
-                    return playerPositionMono;
-                }
-                builder.RegisterInstance(new PlayerPositionMonoBuilder(builder, SetUpPlayerPositionMono()));
-                builder.RegisterInstance(playerParamManager);
-                builder.Register<PlayerParamDataContainer>(Lifetime.Scoped).As<IPlayerParamDataContainer>();
-                builder.Register<PlayerHpParamData>(Lifetime.Scoped).As<IPlayerHpParamData>();
-                builder.Register<PlayerAntiCommentParamData>(Lifetime.Scoped).As<IPlayerAntiCommentParamData>();
-                // Player
-                builder.Register<PlayerPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<PlayerMono>>();
-                builder.Register<PlayerHolder>(Lifetime.Scoped);
-                builder.Register<PlayerAttackedCounter>(Lifetime.Scoped);
-                builder.Register<IStart, PlayerSpawner>(Lifetime.Scoped);
-                // Attack
-                builder.Register<PlayerAttackEffectPrefabLoader>(Lifetime.Scoped)
-                    .As<IPrefabLoader<PlayerAttackEffectMono>>();
-                builder.Register<PlayerAttackEffectSpawner>(Lifetime.Scoped);
-                builder.Register<PlayerAttackEffectBuilder>(Lifetime.Scoped);
-                builder.Register<AttackExecutor>(Lifetime.Transient).As<IAttackExecutor>();
-            }
-
+        public static void RegisterCombo(IContainerBuilder builder, ComboParamManager comboParamManager)
+        {
+            // Parameters
+            builder.RegisterInstance(comboParamManager);
             // Combo
-            RegisterCombo(builder, comboParamManager);
-            static void RegisterCombo(IContainerBuilder builder, ComboParamManager comboParamManager)
-            {
-                // Parameters
-                builder.RegisterInstance(comboParamManager);
-                // Combo
-                builder.Register<ComboMultiplier>(Lifetime.Scoped).As<IComboMultiplier>();
-                builder.Register<ComboCounter>(Lifetime.Scoped);
-                builder.RegisterComponentInHierarchy<ComboViewMono>();
-            }
+            builder.Register<ComboMultiplier>(Lifetime.Scoped).As<IComboMultiplier>();
+            builder.Register<ComboCounter>(Lifetime.Scoped);
+            builder.RegisterComponentInHierarchy<ComboViewMono>();
+        }
 
+        public static void RegisterTower(IContainerBuilder builder, TowerParams towerParams)
+        {
+            // Parameters
+            builder.Register<TowerParamsConfig>(Lifetime.Scoped);
+            builder.RegisterComponentInHierarchy<TowerPositionMono>();
+            builder.RegisterInstance(towerParams);
             // Tower
-            RegisterTower(builder);
-            static void RegisterTower(IContainerBuilder builder)
-            {
-                builder.Register<TowerPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<TowerMono>>();
-                builder.Register<IStart, TowerSpawner>(Lifetime.Scoped);
-            }
+            builder.Register<TowerPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<TowerMono>>();
+            builder.Register<IStart, TowerSpawner>(Lifetime.Scoped);
+        }
 
-            // Enemy
-            RegisterEnemy(builder, enemyParamManager);
-            static void RegisterEnemy(IContainerBuilder builder, EnemyParamManager enemyParamManager)
+        public static void RegisterEnemy(IContainerBuilder builder, EnemyParamManager enemyParamManager)
+        {
+            // Parameters
+            EnemyPositionMono SetUpEnemyPositionMono()
             {
-                // Parameters
-                EnemyPositionMono SetUpEnemyPositionMono()
+                var lanePositionMono = FindObjectOfType<LanePositionMono>();
+                var enemyPositionMono = new GameObject().AddComponent<EnemyPositionMono>();
+
+                foreach (var laneContainer in lanePositionMono.lanePositionContainers)
                 {
-                    var lanePositionMono = FindObjectOfType<LanePositionMono>();
-                    var enemyPositionMono = new GameObject().AddComponent<EnemyPositionMono>();
-
-                    foreach (var laneContainer in lanePositionMono.lanePositionContainers)
-                    {
-                        var enemyContainer = new EnemySpawnedPositionContainer();
-                        foreach (var lanePosition in laneContainer.lanePositions)
-                            enemyContainer.enemySpawnedPoints.Add(lanePosition.enemySpawnedPosition);
-                        enemyPositionMono.enemySpawnedPositionContainers.Add(enemyContainer);
-                    }
-
-                    enemyPositionMono.enemyDespawnedPoint = lanePositionMono.enemyDespawnedPoint;
-                    return enemyPositionMono;
+                    var enemyContainer = new EnemySpawnedPositionContainer();
+                    foreach (var lanePosition in laneContainer.lanePositions)
+                        enemyContainer.enemySpawnedPoints.Add(lanePosition.enemySpawnedPosition);
+                    enemyPositionMono.enemySpawnedPositionContainers.Add(enemyContainer);
                 }
-                builder.RegisterComponent(SetUpEnemyPositionMono());
-                builder.RegisterInstance(enemyParamManager);
-                builder.RegisterInstance(enemyParamManager.enemyLevelDesignParam);
-                builder.Register<EnemySpawnPoint>(Lifetime.Scoped).As<IEnemySpawnPoint>();
-                builder.Register<EnemyTimeLineParamContainer>(Lifetime.Scoped).As<IEnemyTimeLineParamContainer>();
-                builder.Register<EnemyParamDataContainer>(Lifetime.Scoped).AsImplementedInterfaces();
-                builder.RegisterInstance(
-                    new EnemyLevelDesignParamDataBuilder(builder, enemyParamManager.enemyLevelDesignParam));
-                // Enemy
-                builder.Register<EnemyPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<EnemyMono>>();
-                builder.Register<EnemySpawner>(Lifetime.Scoped).AsImplementedInterfaces().AsSelf();
-                builder.Register<EnemyBuilder>(Lifetime.Scoped).As<IEnemyBuilder>();
-                builder.Register<EnemyCluster>(Lifetime.Scoped);
-                builder.Register<EnemyAttackDecider>(Lifetime.Scoped);
-                builder.Register<EnemyTotemOnAttack>(Lifetime.Scoped);
-                builder.Register<EnemyHighlightUpdater>(Lifetime.Scoped).AsImplementedInterfaces();
 
+                enemyPositionMono.enemyDespawnedPoint = lanePositionMono.enemyDespawnedPoint;
+                return enemyPositionMono;
             }
 
-            // Viewer
-            builder.RegisterComponentInHierarchy<ViewerViewMono>();
-
+            builder.RegisterComponent(SetUpEnemyPositionMono());
+            builder.RegisterInstance(enemyParamManager);
+            builder.RegisterInstance(enemyParamManager.enemyLevelDesignParam);
+            builder.Register<EnemySpawnPoint>(Lifetime.Scoped).As<IEnemySpawnPoint>();
+            builder.Register<EnemyTimeLineParamContainer>(Lifetime.Scoped).As<IEnemyTimeLineParamContainer>();
+            builder.Register<EnemyParamDataContainer>(Lifetime.Scoped).AsImplementedInterfaces();
+            builder.RegisterInstance(
+                new EnemyLevelDesignParamDataBuilder(builder, enemyParamManager.enemyLevelDesignParam));
+            // Enemy
+            builder.Register<EnemyPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<EnemyMono>>();
+            builder.Register<EnemySpawner>(Lifetime.Scoped).AsImplementedInterfaces().AsSelf();
+            builder.Register<EnemyBuilder>(Lifetime.Scoped).As<IEnemyBuilder>();
+            builder.Register<EnemyCluster>(Lifetime.Scoped);
+            builder.Register<EnemyAttackDecider>(Lifetime.Scoped);
+            builder.Register<EnemyTotemOnAttack>(Lifetime.Scoped);
+            builder.Register<EnemyHighlightUpdater>(Lifetime.Scoped).AsImplementedInterfaces();
+        }
+        public static void RegisterIrritated(IContainerBuilder builder, IrritatedParams irritatedParams)
+        {
+            // Parameters
+            builder.RegisterInstance(irritatedParams);
             // IrritatedGauge
             builder.RegisterComponentInHierarchy<IrritatedViewMono>();
             builder.RegisterComponentInHierarchy<IrritatedGaugeBackgroundViewMono>();
-            builder.RegisterInstance(irritatedParams);
             builder.Register<IrritatedValue>(Lifetime.Scoped).WithParameter("maxValue", 100);
-
+        }
+        public static void RegisterView(IContainerBuilder builder)
+        {
+            // Viewer
+            builder.RegisterComponentInHierarchy<ViewerViewMono>();
 
             // View
             builder.RegisterComponentInHierarchy<StreamViewMono>();
@@ -205,27 +196,53 @@ namespace Daipan.Daipan
             // ShakeDisplay
             builder.RegisterComponentInHierarchy<ShakeDisplayMono>();
 
+        }
+
+        public static void RegisterBattle(IContainerBuilder builder)
+        {
             // Battle
-            builder.Register<EndSceneSelector>(Lifetime.Scoped).As<IStart>().AsSelf();
-
-
-            // Parameters
-
-
             builder.Register<WaveState>(Lifetime.Scoped);
+            builder.Register<EndSceneSelector>(Lifetime.Scoped).As<IStart>().AsSelf();
+        }
 
-
-            /*tower*/
-            builder.Register<TowerParamsConfig>(Lifetime.Scoped);
-            builder.RegisterComponentInHierarchy<TowerPositionMono>();
-            builder.RegisterInstance(towerParams);
-
+        public static void RegisterInputSerial(IContainerBuilder builder)
+        {
             // InputSerial
             builder.Register<SerialInput>(Lifetime.Scoped);
             builder.Register<InputSerialManager>(Lifetime.Scoped);
+        }
 
-            // Timer
-            builder.Register<StreamTimer>(Lifetime.Scoped).AsSelf().As<IStart>().As<IUpdate>();
+        protected override void Configure(IContainerBuilder builder)
+        {
+            // Stream
+            RegisterStream(builder, streamParam);
+
+            // Comment
+            RegisterComment(builder, commentParamManager);
+
+            // Player
+            RegisterPlayer(builder, playerParamManager);
+
+            // Combo
+            RegisterCombo(builder, comboParamManager);
+
+            // Tower
+            RegisterTower(builder, towerParams);
+
+            // Enemy
+            RegisterEnemy(builder, enemyParamManager);
+
+            // Irritated
+            RegisterIrritated(builder, irritatedParams);
+            
+            // View
+            RegisterView(builder);
+
+            // Battle
+            RegisterBattle(builder);
+            
+            // InputSerial  
+            RegisterInputSerial(builder);
 
             // Updater
             builder.UseEntryPoints(Lifetime.Scoped, entryPoints =>
@@ -233,8 +250,7 @@ namespace Daipan.Daipan
                 entryPoints.Add<Starter>();
                 entryPoints.Add<Updater>();
             });
-
-
+            
             // Debug
             RegisterDebugInput(builder);
         }
