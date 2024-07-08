854d8fb3dce7272aaa66c8007d2e7bcd82ec9931
suupia <suupia771@gmail.com>
2024-07-09 00:03:23 +0900

    Comment out PlayerAttackEffectMoveExecutor

--- Assets/Daipan/Player/Scripts/PlayerAttackEffectMoveExecutor.cs
+++ Assets/Daipan/Player/Scripts/PlayerAttackEffectMoveExecutor.cs
@@ -8,54 +8,60 @@ using UnityEngine;
 
 namespace Daipan.Player.Scripts
 {
-    public class PlayerAttackEffectMoveExecutor
-    {
-        // readonly double _speed = 30;
-        // readonly double _hitDistance = 1.0;
-        // public event EventHandler<OnHitEventArgs>? OnHit;
-        // IPlayerParamData? _playerParamData;
-        // Func<EnemyMono?> _getNearestEnemyMono = () => null;
-        //
-        //
-        // public void Move()
-        // {
-        //     // [Precondition]
-        //     if (_playerParamData == null)
-        //     {
-        //         Debug.LogWarning("PlayerAttackEffectMono: PlayerParamData is null");
-        //         return;
-        //     }
-        //
-        //     var enemyMono = _getNearestEnemyMono();
-        //
-        //     if(enemyMono != null)
-        //     {
-        //         if (!PlayerAttackModule.IsInScreenEnemy(enemyMono))
-        //         {
-        //             enemyMono = null;
-        //         }
-        //         
-        //     }
-        //
-        //     Direction = enemyMono != null ? (enemyMono.transform.position - transform.position).normalized : Direction;
-        //     transform.position += Direction * (float)(_speed * Time.deltaTime);
-        //     if (enemyMono != null)
-        //     {
-        //         if (enemyMono.transform.position.x - transform.position.x < _hitDistance)
-        //         {
-        //             var isTargetEnemy = PlayerAttackModule.GetTargetEnemyEnum(_playerParamData.PlayerEnum())
-        //                 .Contains(enemyMono.EnemyEnum);
-        //             OnHit?.Invoke(this, new OnHitEventArgs(enemyMono, isTargetEnemy));
-        //             Destroy(gameObject);
-        //
-        //         }
-        //     }
-        //     else
-        //     {
-        //         if (transform.position.x > 10) // todo: parameterからもらう
-        //             Destroy(gameObject);
-        //     }
-        // }
-    } 
+    // public class PlayerAttackEffectMoveExecutor
+    // {
+    //     readonly double _speed = 30;
+    //     readonly double _hitDistance = 1.0;
+    //     public event EventHandler<OnHitEventArgs>? OnHit;
+    //     IPlayerParamData? _playerParamData;
+    //     Func<EnemyMono?> _getNearestEnemyMono = () => null;
+    //     Vector3 Direction { get; set; } = Vector3.right;
+    //
+    //     PlayerAttackEffectMono? _playerAttackEffectMono;
+    //     public void Move()
+    //     {
+    //         if (_playerAttackEffectMono == null) return;
+    //         
+    //         // [Precondition]
+    //         if (_playerParamData == null)
+    //         {
+    //             Debug.LogWarning("PlayerAttackEffectMono: PlayerParamData is null");
+    //             return;
+    //         }
+    //     
+    //         var enemyMono = _getNearestEnemyMono();
+    //         enemyMono = PlayerAttackModule.IsInScreenEnemy(enemyMono) ? enemyMono : null;
+    //     
+    //         Direction = enemyMono != null ? (enemyMono.transform.position - _playerAttackEffectMono.transform.position).normalized : Direction;
+    //         _playerAttackEffectMono.transform.position += Direction * (float)(_speed * Time.deltaTime);
+    //         if (enemyMono != null)
+    //         {
+    //             if (enemyMono.transform.position.x - _playerAttackEffectMono.transform.position.x < _hitDistance)
+    //             {
+    //                 var isTargetEnemy = PlayerAttackModule.GetTargetEnemyEnum(_playerParamData.PlayerEnum())
+    //                     .Contains(enemyMono.EnemyEnum);
+    //                 OnHit?.Invoke(this, new OnHitEventArgs(enemyMono, isTargetEnemy));
+    //                 UnityEngine.Object.Destroy(_playerAttackEffectMono.gameObject);
+    //     
+    //             }
+    //         }
+    //         else
+    //         {
+    //             if (_playerAttackEffectMono.transform.position.x > 10) // todo: parameterからもらう
+    //                 UnityEngine.Object.Destroy(_playerAttackEffectMono.gameObject);
+    //         }
+    //     }
+    //     
+    //     public void SetDomain(
+    //         PlayerAttackEffectMono playerAttackEffectMono
+    //         , IPlayerParamData playerParamData
+    //         , Func<EnemyMono?> getTargetEnemyMono
+    //         )
+    //     {
+    //         _playerAttackEffectMono = playerAttackEffectMono;
+    //         _playerParamData = playerParamData;
+    //         _getNearestEnemyMono = getTargetEnemyMono;
+    //     }
+    // } 
 }
 
