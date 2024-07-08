b87d575858d70b16500c6b3d2d8e53428af00603
suupia <suupia771@gmail.com>
2024-07-08 11:53:48 +0900

    Refactor DaipanExecutor

--- Assets/Daipan/Stream/Scripts/DaipanExecutor.cs
+++ Assets/Daipan/Stream/Scripts/DaipanExecutor.cs
@@ -10,35 +10,22 @@ namespace Daipan.Stream.Scripts
 {
     public sealed class DaipanExecutor
     {
-        readonly CommentCluster _commentCluster;
         readonly AntiCommentCluster _antiCommentCluster;
-        readonly DaipanParam _daipanParam;
         readonly EnemyCluster _enemyCluster;
         readonly IrritatedValue _irritatedValue;
-        readonly StreamStatus _streamStatus;
-        readonly ViewerNumber _viewerNumber;
         readonly StreamerViewMono _streamerViewMono;
         readonly ShakeDisplayMono _shakeDisplayMono;
-        public int DaipanNum = 0;
 
         public DaipanExecutor(
-            DaipanParam daipanParam,
-            ViewerNumber viewerNumber,
-            StreamStatus streamStatus,
             IrritatedValue irritatedValue,
             EnemyCluster enemyCluster,
-            CommentCluster commentCluster,
             AntiCommentCluster antiCommentCluster,
             StreamerViewMono streamerViewMono,
             ShakeDisplayMono shakeDisplayMono
         )
         {
-            _daipanParam = daipanParam;
-            _viewerNumber = viewerNumber;
-            _streamStatus = streamStatus;
             _irritatedValue = irritatedValue;
             _enemyCluster = enemyCluster;
-            _commentCluster = commentCluster;
             _antiCommentCluster = antiCommentCluster;
             _streamerViewMono = streamerViewMono;
             _shakeDisplayMono = shakeDisplayMono;
@@ -53,7 +40,6 @@ namespace Daipan.Stream.Scripts
                 _antiCommentCluster.BlownAway();
                 _streamerViewMono.Daipan();
                 _shakeDisplayMono.Daipan();
-                DaipanNum++;
                 
                 // 台パンしたら怒りゲージは0になる
                 _irritatedValue.DecreaseValue(_irritatedValue.Value);
