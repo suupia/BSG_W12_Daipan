73782df9ec1509acc41bfb8212ee056137e9c45b
suupia <suupia771@gmail.com>
2024-07-11 12:00:35 +0900

    Add PlayerOnAttackedTutorial.cs

--- Assets/Daipan/Player/Scripts/PlayerOnAttackedTutorial.cs
+++ Assets/Daipan/Player/Scripts/PlayerOnAttackedTutorial.cs
@@ -4,44 +4,22 @@ using System.Linq;
 using Daipan.Comment.Scripts;
 using Daipan.Enemy.Interfaces;
 using Daipan.Player.Interfaces;
-using Daipan.Stream.Scripts;
 using UnityEngine;
 
 namespace Daipan.Player.Scripts
 {
-    public class PlayerOnAttacked : IPlayerOnAttacked
+   
+    public class PlayerOnAttackedTutorial : IPlayerOnAttacked
     {
-        readonly IrritatedValue _irritatedValue;
-        readonly PlayerAttackedCounter _playerAttackedCounter;
-        readonly CommentSpawner _commentSpawner;
         List<AbstractPlayerViewMono?>? _playerViewMonos; 
-        public PlayerOnAttacked
-        (
-            IrritatedValue irritatedValue
-            , PlayerAttackedCounter playerAttackedCounter
-            , CommentSpawner commentSpawner
-        )
-        {
-            _irritatedValue = irritatedValue;
-            _playerAttackedCounter = playerAttackedCounter;
-            _commentSpawner = commentSpawner;
-        }
-
+        
         public void SetPlayerViews(List<AbstractPlayerViewMono?> playerViewMonos)
         {
             _playerViewMonos = playerViewMonos;
         }
-
         public Hp OnAttacked(Hp hp, IEnemyParamData enemyParamData)
         {
-            // イライラゲージ
-            _irritatedValue.IncreaseValue(enemyParamData.GetAttackAmount());
-            
-            // アンチコメント
-            _playerAttackedCounter.CountUp();
-            if (_playerAttackedCounter.IsOverThreshold)
-                _commentSpawner.SpawnCommentByType(CommentEnum.Spiky);
-            
+                        
             // view
             if (_playerViewMonos == null)
             {
@@ -56,7 +34,5 @@ namespace Daipan.Player.Scripts
             }            
             return new Hp(hp.Value - enemyParamData.GetAttackAmount());
         }
-
     } 
 }
-
