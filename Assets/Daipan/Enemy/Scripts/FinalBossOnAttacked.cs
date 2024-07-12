23ea8c9c2a11f3d4c336ffbd8dfaaa85785de15f
suupia <suupia771@gmail.com>
2024-07-13 00:23:43 +0900

    Add temp FinalBossOnAttacked

---
+++ Assets/Daipan/Enemy/Scripts/FinalBossOnAttacked.cs
@@ -0,0 +1,43 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Daipan.Enemy.Interfaces;
+using Daipan.Enemy.MonoScripts;
+using Daipan.Player.LevelDesign.Interfaces;
+using Daipan.Player.MonoScripts;
+using Daipan.Player.Scripts;
+using Daipan.Stream.Scripts;
+using R3;
+using UnityEngine;
+
+namespace Daipan.Enemy.Scripts
+{
+    public sealed class FinalBossOnAttacked : IEnemyOnAttacked
+    {
+        const double AllowableSec = 0.15f;
+        readonly SamePressChecker _samePressChecker;
+        readonly List<PlayerColor> _canAttackPlayers;
+
+        public FinalBossOnAttacked(List<PlayerColor> canAttackPlayers)
+        {
+           _samePressChecker = new SamePressChecker(AllowableSec, canAttackPlayers.Count); 
+           _canAttackPlayers = canAttackPlayers;
+        }
+
+
+        public Hp OnAttacked(Hp hp, IPlayerParamData playerParamData)
+        {
+            Debug.Log($"OnAttacked hp: { hp.Value } playerParamData: { playerParamData }");
+            var attackPlayer = playerParamData.PlayerEnum();
+            var index = _canAttackPlayers.IndexOf(attackPlayer);
+            if (index == -1) return hp;
+            _samePressChecker.SetOn(index);
+            if (!_samePressChecker.IsAllOn()) return hp;
+            return new Hp(hp.Value - playerParamData.GetAttack());
+        }
+        
+    }
+
+ 
+}
