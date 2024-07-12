56e6c2bcbdf9a3e1744125274a189746936350b3
suupia <suupia771@gmail.com>
2024-07-12 13:50:22 +0900

    Add FinalBossBuilder.cs

---
+++ Assets/Daipan/Enemy/Scripts/FinalBossBuilder.cs
@@ -0,0 +1,14 @@
+#nullable enable
+using Daipan.Enemy.MonoScripts;
+
+namespace Daipan.Enemy.Scripts
+{
+    public class FinalBossBuilder
+    {
+        public FinalBossMono Build(FinalBossMono finalBossMono)
+        {
+            return finalBossMono;
+        }
+    } 
+}
+
