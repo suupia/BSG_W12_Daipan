2bb75d080ebd820d002bd0b42700eada2e210ecb
suupia <suupia771@gmail.com>
2024-07-12 22:05:55 +0900

    Change summon action interval sec

--- Assets/Daipan/Enemy/Interfaces/IEnemyMonoDie.cs
+++
@@ -1,9 +0,0 @@
-#nullable enable
-namespace Daipan.Enemy.Interfaces
-{
-    public interface IEnemyMonoDie
-    {
-        void Die(AbstractEnemyMono enemyMono, bool isDaipaned = false);
-    } 
-}
-
