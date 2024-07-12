4d5f7b548c0ddfd86b495762ce8f5353c0528682
suupia <suupia771@gmail.com>
2024-07-13 01:21:18 +0900

    Add FinalBossColorParam

--- Assets/Daipan/Enemy/LevelDesign/Scripts/FinalBossParam.cs
+++ Assets/Daipan/Enemy/LevelDesign/Scripts/FinalBossParam.cs
@@ -1,5 +1,7 @@
 #nullable enable
 using System;
+using System.Collections.Generic;
+using Daipan.Enemy.Scripts;
 using Daipan.LevelDesign.Enemy.Scripts;
 using UnityEngine;
 
@@ -13,10 +15,21 @@ namespace Daipan.Enemy.LevelDesign.Scripts
 
         [Header("FinalBOSS固有のパラメータを設定してください。")]
         public double summonActionIntervalSec = 1;
-
         public double summonEnemyIntervalSec = 1;
         public int summonEnemyCount = 5;
+        
+        [Header("FinalBOSSの色のパラメータを設定してください。")]
+        List<FinalBossColorParam> finalBossColorParams = null!;
     }
-
+    
+    [Serializable]
+    public sealed class FinalBossColorParam
+    {
+        public FinalBossColor finalBossColor;
+        public Color bodyColor = Color.white;
+        public Color eyeColor = new(226f / 255f, 248f / 255f, 227f / 255f);
+        public Color eyeBallColor = Color.white;
+        public Color lineColor = new(111f / 255f, 87f / 255f, 107f / 255f);
+    }
 }
 
