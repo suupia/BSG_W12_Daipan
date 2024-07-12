8d42f1173b8dc96a497d041d666ec6c921c33ce1
suupia <suupia771@gmail.com>
2024-07-13 02:57:38 +0900

    Add DaipanHit to Base_FinalBoss_Motion

--- Assets/Daipan/Enemy/Interfaces/AbstractFinalBossViewMono.cs
+++ Assets/Daipan/Enemy/Interfaces/AbstractFinalBossViewMono.cs
@@ -12,6 +12,7 @@ namespace Daipan.Enemy.Interfaces
         public abstract void Attack();
         public abstract void Died(Action onDied);
         public abstract void Daipaned(Action onDaipaned);
+        public abstract void DaipanHit();
         public abstract void Highlight(bool isHighlighted);
         
         // FinalBoss
