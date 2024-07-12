51becc91ae2b4002cb573e8878125a0dc3299d25
suupia <suupia771@gmail.com>
2024-07-12 17:06:35 +0900

    Add EnemyParam process to FinalBossParamData

--- Assets/Daipan/Enemy/Scripts/FinalBossParamData.cs
+++ Assets/Daipan/Enemy/Scripts/FinalBossParamData.cs
@@ -6,31 +6,31 @@ namespace Daipan.Enemy.Scripts
 {
     public class FinalBossParamData : IFinalBossParamData, IFinalBossViewParamData
     {
-        readonly FinalBossParamManager _finalBossParamManager;
+        readonly FinalBossParam _finalBossParam;
         
         public FinalBossParamData(FinalBossParamManager finalBossParamManager)
         {
-            _finalBossParamManager = finalBossParamManager;
+            _finalBossParam = finalBossParamManager.finalBossParam;
         }
 
 
         public EnemyEnum GetEnemyEnum() => EnemyEnum.None;
 
         // Attack
-        public int GetAttackAmount() => 0;
-        public double GetAttackIntervalSec() => 0;
-        public double GetAttackRange() => 0;
+        public int GetAttackAmount() => _finalBossParam.enemyParam.enemyAttackParam.attackAmount;
+        public double GetAttackIntervalSec() => _finalBossParam.enemyParam.enemyAttackParam.attackIntervalSec;
+        public double GetAttackRange() => _finalBossParam.enemyParam.enemyAttackParam.attackRange;
 
         // Hp
-        public int GetMaxHp() => 0;
+        public int GetMaxHp() => _finalBossParam.enemyParam.enemyHpParam.maxHp;
 
         // Move
-        public double GetMoveSpeedPerSec() => 0;
+        public double GetMoveSpeedPerSec() => _finalBossParam.enemyParam.enemyMoveParam.moveSpeedPerSec;
         
         // FinalBoss
-        public double GetSummonActionIntervalSec() => _finalBossParamManager.finalBossParam.summonActionIntervalSec;
-        public int GetSummonEnemyCount() => _finalBossParamManager.finalBossParam.summonEnemyCount;
-        public double GetSummonEnemyIntervalSec() => _finalBossParamManager.finalBossParam.summonEnemyIntervalSec;
+        public double GetSummonActionIntervalSec() => _finalBossParam.summonActionIntervalSec;
+        public int GetSummonEnemyCount() => _finalBossParam.summonEnemyCount;
+        public double GetSummonEnemyIntervalSec() => _finalBossParam.summonEnemyIntervalSec;
      } 
 }
 
