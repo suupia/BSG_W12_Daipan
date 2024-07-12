5dd210fba63ea27c0e21dd3031eb42f688d7e14d
suupia <suupia771@gmail.com>
2024-07-12 16:50:57 +0900

    Add FinalBossViewMono

--- Assets/Daipan/Enemy/Interfaces/IFinalBossViewParamData.cs
+++ Assets/Daipan/Enemy/Interfaces/IFinalBossViewParamData.cs
@@ -1,9 +1,15 @@
 #nullable enable
+using UnityEngine;
+
 namespace Daipan.Enemy.Interfaces
 {
     public interface IFinalBossViewParamData
     {
-    
+        // Colors
+        Color GetBodyColor() => Color.white;
+        Color GetEyeColor() => Color.white;
+        Color GetEyeBallColor() => Color.white;
+        Color GetLineColor() => Color.white; 
     } 
 }
 
