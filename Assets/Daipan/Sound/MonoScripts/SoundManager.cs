f2286b57ec56c08f287520236cdfb705d66fe002
suupia <suupia771@gmail.com>
2024-07-14 11:34:56 +0900

    Add temp SoundManager

---
+++ Assets/Daipan/Sound/MonoScripts/SoundManager.cs
@@ -0,0 +1,46 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Daipan.Sound.MonoScripts
+{
+    public sealed class SoundManager : MonoBehaviour
+    {
+        [SerializeField] List<SeParam> seParams = null!;
+        
+        AudioSource audioSource = null!;
+
+        void Awake()
+        {
+            audioSource = gameObject.AddComponent<AudioSource>(); 
+        }
+
+
+        public void PlaySe(SeEnum seEnum)
+        {
+            var seParam = seParams.Find(x => x.seEnum == seEnum);
+            if (seParam == null)
+            {
+                Debug.LogError($"Not found SE: {seEnum}");
+                return;
+            }
+
+            audioSource.clip = seParam.audioClip;
+            audioSource.Play();
+        }
+    }
+
+    [Serializable]
+    public sealed class SeParam
+    {
+        public SeEnum seEnum;
+        public AudioClip audioClip = null!;
+    }
+
+    public enum SeEnum
+    {
+        SpawnComment,
+        SpawnAntiComment,
+    }
+}
