357b7bf06a141727174d1d7c99921815818b6626
suupia <suupia771@gmail.com>
2024-07-15 00:55:18 +0900

    Fix Header of finalBossAntiCommentPercentOnMissAttacks

--- Assets/Daipan/Player/LevelDesign/Scripts/PlayerParam.cs
+++ Assets/Daipan/Player/LevelDesign/Scripts/PlayerParam.cs
@@ -36,14 +36,15 @@ namespace Daipan.Player.LevelDesign.Scripts
         [Header("n回攻撃されたらアンチコメント")]
         [Min(0)]
         public int antiCommentThreshold;
-        [Header("Waveごとの、異なる敵に攻撃をしてしまった時にアンチコメントが生成される確率")]
-        public List<double> antiCommentPercentOnMissAttacks = null!;
 
         [Header("n回攻撃をミスしたらアンチコメントが生成される")] [Min(0)]
         public int missedAttackCountForAntiComments = 10;
 
-        [Header("n回ラスボスに攻撃をミスしたらアンチコメントが生成される")] [Min(0)]
-        public int finalBossAntiCommentPercentOnMissAttacks = 5;
+        [Header("Waveごとの、異なる敵に攻撃をしてしまった時にアンチコメントが生成される確率")]
+        public List<double> antiCommentPercentOnMissAttacks = null!;
+
+        [Header("ラスボスに異なる攻撃をしてしまったときにアンチコメントが生成される確率")] [Min(0)]
+        public int finalBossAntiCommentPercentOnMissAttacks = 30;
 
     }
 
