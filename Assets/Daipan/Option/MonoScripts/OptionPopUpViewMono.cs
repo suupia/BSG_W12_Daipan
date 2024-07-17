ca141e50b2e413a789db3584e16935ba30d9b16a
aya097 <akase097@gmail.com>
2024-07-17 19:16:14 +0900

    add OptionUIObject

---
+++ Assets/Daipan/Option/MonoScripts/OptionPopUpViewMono.cs
@@ -0,0 +1,34 @@
+#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Daipan.Option.Scripts;
+using VContainer;
+
+namespace Daipan.Option.MonoScripts
+{
+    public class OptionPopUpViewMono : MonoBehaviour
+    {
+        [SerializeField] GameObject mainOptionCanvas = null!;
+
+
+
+        [Inject]
+
+        public void Initialize()
+        {
+            
+        }
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
