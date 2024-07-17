f09e4de8d4b85a89f36c23c50792dcc513407466
aya097 <akase097@gmail.com>
2024-07-17 23:00:15 +0900

    add input like getkey with serial

--- Assets/Daipan/InputSerial/Scripts/InputSerialManager.cs
+++ Assets/Daipan/InputSerial/Scripts/InputSerialManager.cs
@@ -15,6 +15,10 @@ namespace Daipan.InputSerial.Scripts
         bool[] _isFirstInputs = new bool[30];
         bool[] _isDuringPress = new bool[30];
 
+        bool[] _isPressing = new bool[30];
+
+
+
         [Inject]
         public void Initialize(SerialInput serialInput)
         {
@@ -26,6 +30,10 @@ namespace Daipan.InputSerial.Scripts
             for (int i = 0; i < _isDuringPress.Length; i++)
             {
                 _isDuringPress[i] = false;
+            }
+            for (int i = 0; i < _isPressing.Length; i++)
+            {
+                _isPressing[i] = false;
             }
         }
 
@@ -54,7 +62,28 @@ namespace Daipan.InputSerial.Scripts
         {
             return GetButtonRed() || GetButtonBlue() || GetButtonYellow();
         }
+
         // ここまで
+        public bool GetButtonPressingRed()
+        {
+            return getInputContinue(0)
+                || Input.GetKey(KeyCode.W);
+        }
+        public bool GetButtonPressingBlue()
+        {
+            return getInputContinue(1)
+                || Input.GetKey(KeyCode.A);
+        }
+        public bool GetButtonPressingYellow()
+        {
+            return getInputContinue(2)
+                || Input.GetKey(KeyCode.S);
+        }
+        public bool GetButtonPressingMenu()
+        {
+            return getInputContinue(3)
+                || Input.GetKey(KeyCode.Tab);
+        }
 
 
 
@@ -87,6 +116,32 @@ namespace Daipan.InputSerial.Scripts
                 {
                     _isDuringPress[digit] = false;
                 });
+
+            return true;
+        }
+
+        // 重複する部分もあるが、バグが怖いので新しく書く
+        bool getInputContinue(int digit)
+        {
+            // シリアルポートが有効か？
+            if (!isSerial()) return false;
+
+            // 押されている途中ならtrue
+            if (_isPressing[digit]) return true;
+
+            // 受け取った入力がT/Fか？
+            if ((_serialInput.number & 1 << digit) == 0)
+            {
+                _isPressing[digit] = false;
+                return false;
+            }
+
+            // 一度trueになったら_chatteringSecの間trueを返す
+            _isPressing[digit] = true;
+            Observable.Timer(TimeSpan.FromSeconds(_chatteringSec)).Subscribe(_ =>
+            {
+                _isPressing[digit] = false;
+            });
 
             return true;
         }
