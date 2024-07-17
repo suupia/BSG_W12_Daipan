6a8188b53e8be8a8fbc284cb9d9797826ba4fe27
suupia <suupia771@gmail.com>
2024-07-17 18:10:19 +0900

    Add ResultUI

--- Assets/Daipan/Battle/Scripts/ResultViewMono.cs
+++ Assets/Daipan/Battle/Scripts/ResultViewMono.cs
@@ -13,10 +13,14 @@ namespace Daipan.Battle.scripts
     public class ResultViewMono : MonoBehaviour
     {
         [SerializeField] GameObject viewObject = null!;
+        [SerializeField] TextMeshProUGUI viewerNumberExplainText = null!;
         [SerializeField] TextMeshProUGUI viewerNumberText = null!;
-        [SerializeField] TextMeshProUGUI comboCountText = null!;
+        [SerializeField] TextMeshProUGUI daipanCountExplainText = null!;
         [SerializeField] TextMeshProUGUI daipanCountText = null!;
+        [SerializeField] TextMeshProUGUI playerHpExplainText = null!;
         [SerializeField] TextMeshProUGUI playerHpText = null!;
+        [SerializeField] TextMeshProUGUI comboCountExplainText = null!;
+        [SerializeField] TextMeshProUGUI comboCountText = null!;
         [SerializeField] TextMeshProUGUI tankYouText = null!;
         [SerializeField] TextMeshProUGUI pushEnterText = null!;
 
@@ -35,15 +39,15 @@ namespace Daipan.Battle.scripts
             Debug.Log("ResultViewMono Constructor");
             Observable.EveryUpdate()
                 .Where(_ => viewObject.activeInHierarchy)
-                .Subscribe(_ => viewerNumberText.text = $"Viewer Number: {viewerNumber.Number}")
+                .Subscribe(_ => viewerNumberText.text = $"{viewerNumber.Number}")
                 .AddTo(this);
             Observable.EveryUpdate()
                 .Where(_ => viewObject.activeInHierarchy)
-                .Subscribe(_ => comboCountText.text = $"Combo Count: {comboCounter.MaxComboCount}")
+                .Subscribe(_ => daipanCountText.text = $"{daipanExecutor.DaipanCount}")
                 .AddTo(this);
             Observable.EveryUpdate()
                 .Where(_ => viewObject.activeInHierarchy)
-                .Subscribe(_ => daipanCountText.text = $"Daipan Count: {daipanExecutor.DaipanCount}")
+                .Subscribe(_ => comboCountText.text = $"{comboCounter.MaxComboCount}")
                 .AddTo(this);
         }
         
@@ -55,10 +59,15 @@ namespace Daipan.Battle.scripts
                 Debug.LogWarning("PlayerMono is not found");
                 return;
             }
-            playerHpText.text = $"Player HP :{playerMono.Hp.Value}";  // 本当はObserveしたいけど生成順序の関係でここで取得
+            playerHpText.text = $"{playerMono.Hp.Value} / {playerMono.MaxHp}";  // 本当はObserveしたいけど生成順序の関係でここで取得
+
+            viewerNumberExplainText.text = "同時接続数";
+            daipanCountExplainText.text = "台パン回数";
+            playerHpExplainText.text = "塔の最終のHP";
+            comboCountExplainText.text = "最大コンボ数";
             
             tankYouText.text = "ご視聴いただきありがとうございました！";
-            pushEnterText.text = "Push Enter to see the end scene"; 
+            pushEnterText.text = "次へ"; 
             
             viewObject.SetActive(true);
         }
