d1e24f6c1ebb1eae5c9770f3703817bd3dbbd221
suupia <suupia771@gmail.com>
2024-08-27 08:58:15 +0900

    Refactor TutorialSpeechSpritesMono

--- Assets/Daipan/Tutorial/MonoScripts/TutorialSpeechSpritesMono.cs
+++ Assets/Daipan/Tutorial/MonoScripts/TutorialSpeechSpritesMono.cs
@@ -7,15 +7,14 @@ namespace Daipan.Tutorial.MonoScripts
     [Serializable]
     public class SerializableKeyPair<TKey, TValue>
     {
-        [SerializeField] private TKey key;
-        [SerializeField] private TValue value;
-
+        [SerializeField] TKey key;
+        [SerializeField] TValue value;
         public TKey Key => key;
         public TValue Value => value;
     }
+
     public class TutorialSpeechSpritesMono : MonoBehaviour
     {
-        [SerializeField] SerializableKeyPair<string,Sprite>[] tutorialSprites = null!;
-    } 
+        [SerializeField] SerializableKeyPair<string, Sprite>[] tutorialSprites = null!;
+    }
 }
-
