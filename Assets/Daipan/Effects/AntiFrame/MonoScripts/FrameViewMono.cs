15fbf473f334930a01981268725780861985a1c5
aya097 <akase097@gmail.com>
2024-08-27 21:46:12 +0900

    add select frame by anticommentnum

--- Assets/Daipan/Effects/AntiFrame/MonoScripts/FrameViewMono.cs
+++ Assets/Daipan/Effects/AntiFrame/MonoScripts/FrameViewMono.cs
@@ -13,7 +13,18 @@ namespace Daipan.Effects.MonoScripts
         [SerializeField] Image maxAntiFrame;
 
         [SerializeField] Transform antiCommentParent;
-        
+
+        [Header("アンチコメントがn以上あれば、antiFrame")]
+        [Min(0)]
+        [SerializeField] int antiThreshold;
+
+        [Header("アンチコメントがn以上あれば、maxAntiFrame")]
+        [Min(0)]
+        [SerializeField] int maxAntiThreshold;
+
+
+        int preAntiCommentNum = 0;
+
         void Start()
         {
 
@@ -22,8 +33,47 @@ namespace Daipan.Effects.MonoScripts
         
         void Update()
         {
+            if (antiThreshold >= maxAntiThreshold) Debug.LogError("antiThreshold >= maxAntiThreshold は不正値です。");
             int antiCommentNum = antiCommentParent.childCount;
             //Debug.Log($"antiCommentNum:{antiCommentNum}");
+
+            if (preAntiCommentNum == antiCommentNum) return;
+
+            if(antiCommentNum < antiThreshold)
+            {
+                ShowOriginalFrame();
+            }
+            else if(antiCommentNum < maxAntiThreshold)
+            {
+                ShowAntiFrame();
+            }
+            else
+            {
+                ShowMaxAntiFrame();
+            }
+
+
+            preAntiCommentNum = antiCommentNum;
         }
+
+        void ShowOriginalFrame()
+        {
+            originalFrame.color = Color.white;
+            antiFrame.enabled = false;
+            maxAntiFrame.enabled = false;
+        }
+        void ShowAntiFrame()
+        {
+            originalFrame.color = new Vector4(0.8f, 0.8f, 0.8f, 1f);
+            antiFrame.enabled = true;
+            maxAntiFrame.enabled = false;
+        }
+        void ShowMaxAntiFrame()
+        {
+            originalFrame.color = new Vector4(0.8f, 0.8f, 0.8f, 1f);
+            antiFrame.enabled = true;
+            maxAntiFrame.enabled = true;
+        }
+
     }
 }
