cb6c6077290b4f1d2fac7e59db30f39a254d2326
suupia <suupia771@gmail.com>
2024-08-27 10:21:44 +0900

    Add IEnemyOnDied

---
+++ Assets/Daipan/Enemy/Scripts/NoneEnemyOnDied.cs
@@ -0,0 +1,13 @@
+#nullable enable
+using Daipan.Enemy.Interfaces;
+
+namespace Daipan.Enemy.Scripts
+{
+    public class NoneEnemyOnDied : IEnemyOnDied
+    {
+        public void OnDied()
+        {
+            // Do nothing.
+        }
+    } 
+}
---
+++ Assets/Daipan/Enemy/Scripts/TutorialEnemyOnDied.cs
@@ -0,0 +1,15 @@
+#nullable enable
+using Daipan.Enemy.Interfaces;
+
+namespace Daipan.Enemy.Scripts
+{
+    public class TutorialEnemyOnDied : IEnemyOnDied
+    {
+        public void OnDied()
+        {
+            // todo : 倒された時に、その敵の種類に応じて、チュートリアルのIsSuccessをtrueにする
+            
+        }
+    } 
+}
+
