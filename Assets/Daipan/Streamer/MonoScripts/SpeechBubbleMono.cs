74d7eb1a5de5cfd3a5f560c62681399841cc7db9
suupia <suupia771@gmail.com>
2024-08-27 09:20:26 +0900

    Fix EnqueueSpeechMessage() to show speech image

--- Assets/Daipan/Streamer/MonoScripts/SpeechBubbleMono.cs
+++ Assets/Daipan/Streamer/MonoScripts/SpeechBubbleMono.cs
@@ -9,6 +9,7 @@ using TMPro;
 using UnityEngine;
 using DG.Tweening;
 using R3;
+using UnityEditor.VersionControl;
 using UnityEngine.Assertions;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -20,13 +21,15 @@ namespace Daipan.Streamer.MonoScripts
     {
         // 画像付きの吹き出し
         [SerializeField] Image speechImage = null!;
+
         [SerializeField] TextMeshProUGUI speechWithSpriteText = null!;
+
         // 画像なしの吹き出し
         [SerializeField] TextMeshProUGUI speechText = null!;
 
         const float DurationSec = 0.5f;
         const double MinShowSec = 0.5;
-        readonly Queue<Speech> _speechQueue = new ();
+        readonly Queue<Speech> _speechQueue = new();
         TutorialSpeechSpritesMono _speechSprites = null!;
         public bool IsStartTutorial { get; set; } // Loadingの時に吹き出しが出ないようにするために必要
 
@@ -36,42 +39,33 @@ namespace Daipan.Streamer.MonoScripts
         public void Construct(SpeechEventManager speechEventManager)
         {
             Observable.EveryValueChanged(speechEventManager, x => x.CurrentEvent)
-                .Subscribe(_ =>
-                {
-                    Debug.Log($"speechEventManager : {speechEventManager}");
-                    Debug.Log($"speechEventManager.CurrentEvent : {speechEventManager.CurrentEvent}");
-                    Debug.Log($"speechEventManager.CurrentEvent.Speech : {speechEventManager.CurrentEvent.Speech}");
-                    Debug.Log($"speechEventManager.CurrentEvent.Speech.Message : {speechEventManager.CurrentEvent.Speech.Message}");
-                    EnqueueSpeechMessage(speechEventManager.CurrentEvent.Speech.Message);
-                })
+                .Subscribe(_ => EnqueueSpeechMessage(speechEventManager.CurrentEvent.Speech))
                 .AddTo(this);
         }
 
         void Awake()
         {
             // 初期状態は非表示
-            transform.localScale = Vector3.zero; 
+            transform.localScale = Vector3.zero;
             _speechSprites = FindObjectOfType<TutorialSpeechSpritesMono>();
             Assert.IsNotNull(_speechSprites);
         }
-        
+
         void Update()
         {
             if (!IsStartTutorial) return;
-            
+
             Timer += Time.deltaTime;
 
             if (_speechQueue.Any())
-            {
                 if (Timer > MinShowSec)
                 {
                     ShowSpeechBubble(_speechQueue.Dequeue());
                     Timer = 0;
                 }
-            }
         }
 
-         void ShowSpeechBubble(Speech speech)
+        void ShowSpeechBubble(Speech speech)
         {
             // DoTweenでDoScaleで拡大して表示
             transform.DOScale(Vector3.one, DurationSec).OnComplete(() =>
@@ -85,15 +79,17 @@ namespace Daipan.Streamer.MonoScripts
                 {
                     speechText.text = speech.Message;
                 }
-                if(speech != string.Empty) SoundManager.Instance?.PlaySe(SeEnum.Text);
+
+                if (speech != string.Empty) SoundManager.Instance?.PlaySe(SeEnum.Text);
             });
         }
 
-        public void EnqueueSpeechMessage(string message)
+        void EnqueueSpeechMessage(Speech message)
         {
-            if(_speechQueue.LastOrDefault() == message) return;
+            if (_speechQueue.LastOrDefault() == message) return;
             _speechQueue.Enqueue(message);
         }
+
         public void HideSpeechBubble()
         {
             // DoTweenでDoScaleで縮小して非表示
