5f5768e7b8f56897f01374c796b7e780085db62b
aya097 <akase097@gmail.com>
2024-09-01 11:09:21 +0900

    rotate lings

--- Assets/Daipan/Tutorial/MonoScripts/LanguageSelectionPopupMono.cs
+++ Assets/Daipan/Tutorial/MonoScripts/LanguageSelectionPopupMono.cs
@@ -1,6 +1,10 @@
 #nullable enable
 using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using DG.Tweening;
 
 namespace Daipan.Tutorial.MonoScripts
 {
@@ -8,9 +12,25 @@ namespace Daipan.Tutorial.MonoScripts
     {
         [SerializeField] GameObject popupView = null!;
 
+        // リング
+        [SerializeField]
+        RectTransform satellites;
+        [SerializeField]
+        RectTransform ling;
+        [SerializeField]
+        float rotateSpeed;
+
+
         void Awake()
         {
-            HidePopup(); 
+            HidePopup();
+
+            // リング
+            DOVirtual.Float(0f,0.999f,rotateSpeed, value =>
+            {
+                ActivateSatellites(value);
+                ling.eulerAngles = new Vector3(0, 0, 360f * value);
+            }).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
         }
 
         public void ShowPopup()
@@ -21,6 +41,18 @@ namespace Daipan.Tutorial.MonoScripts
         public void HidePopup()
         {
             popupView.SetActive(false);
+        }
+
+        void ActivateSatellites(float index)
+        {
+            int count = (int)(satellites.childCount * index);
+            for (int i = 0; i < satellites.childCount; i++)
+            {
+                if (i == count)
+                    satellites.GetChild(i).gameObject.SetActive(true);
+                else
+                    satellites.GetChild(i).gameObject.SetActive(false);
+            }
         }
     } 
 }
