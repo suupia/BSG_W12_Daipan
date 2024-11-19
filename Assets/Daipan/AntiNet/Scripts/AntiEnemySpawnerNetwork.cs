84f45c5fc2f9f1b929436ff0c9d98a5e03d13522
aya097 <akase097@gmail.com>
2024-11-19 17:37:45 +0900

    StreamerRPCReceiverNet is became receiving

--- Assets/Daipan/AntiNet/Scripts/AntiEnemySpawnerNetwork.cs
+++ Assets/Daipan/AntiNet/Scripts/AntiEnemySpawnerNetwork.cs
@@ -2,14 +2,26 @@
 using System;
 using UnityEngine;
 using Daipan.Enemy.Scripts;
+using VContainer;
+using Daipna.StreamerNet.Scripts;
 
 namespace Daipan.AntiNet.Scripts
 {
     public class AntiEnemySpawnerNetwork
     {
+        readonly StreamerRPCReceiverNetWrapper _streamerRPCReceiverNetWrapper;
+        [Inject]
+        public AntiEnemySpawnerNetwork(
+            StreamerRPCReceiverNetWrapper streamerRPCReceiverNetWrapper
+        )
+        {
+            _streamerRPCReceiverNetWrapper = streamerRPCReceiverNetWrapper;
+        }
+
         public void SpawnEnemy(EnemyEnum enemyEnum)
         {
             Debug.Log($"Anti spawns {enemyEnum}");
+            _streamerRPCReceiverNetWrapper.RPCReceiverNet.SpawnEnemyRPC(EnemyEnum.None);
         }
     }
 }
