3ff36c8cd0f5a744958f420958569d0c66dcc952
aya097 <akase097@gmail.com>
2024-11-19 20:44:00 +0900

    add SpawnEnemyCostValue

---
+++ Assets/Daipan/AntiNet/Scripts/SpawnEnemyCostValue.cs
@@ -0,0 +1,34 @@
+#nullable enable
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Daipan.AntiNet.Scripts
+{
+    public class SpawnEnemyCostValue
+    {
+        // todo 値をParamsにまとめる
+        readonly int MaxValue = 20;
+        readonly int initializedValue = 5;
+        //
+        int value;
+
+        SpawnEnemyCostValue()
+        {
+            value = initializedValue;
+        }
+
+        public void IncreaseValue(int amount)
+        {
+            if (amount < 0) return;
+            value = Math.Max(MaxValue, value + amount);
+        }
+
+        public void DecreaseValue(int amount)
+        {
+            if (amount < 0) return;
+            value = Math.Min(0, value - amount);
+        }
+    }
+}
