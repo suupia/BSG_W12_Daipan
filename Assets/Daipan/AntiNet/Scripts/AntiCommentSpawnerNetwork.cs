37b7c6d01932de80bdd58dfd1f8cea2f448c722c
aya097 <akase097@gmail.com>
2024-11-21 20:46:02 +0900

    add AntiCommentNet

---
+++ Assets/Daipan/AntiNet/Scripts/AntiCommentSpawnerNetwork.cs
@@ -0,0 +1,12 @@
+#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Daipan.AntiNet.Scripts
+{
+    public class AntiCommentSpawnerNetwork
+    {
+
+    }
+}
---
+++ Assets/Daipan/Comment/MonoScripts/AntiCommentNet.cs
@@ -0,0 +1,69 @@
+#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Fusion;
+using Daipan.Comment.Scripts;
+using Daipan.LevelDesign.Comment.Scripts;
+using VContainer;
+using Daipan.Stream.Scripts;
+
+namespace Daipan.Comment.MonoScripts
+{
+    public class AntiCommentNet : NetworkBehaviour
+    {
+        [SerializeField] TextMeshPro commentText = null!;
+
+        AntiCommentCluster _antiCommentCluster = null!;
+        CommentParamsServer _commentParamsServer = null!;
+        IrritatedGaugeValue _irritatedGaugeValue = null!;
+        bool IsActive { get; set; } = true;
+
+        [Inject]
+        public void Initialize(
+            AntiCommentCluster antiCommentCluster
+            , CommentParamsServer commentParamsServer
+            , IrritatedGaugeValue irritatedGaugeValue
+        )
+        {
+            _antiCommentCluster = antiCommentCluster;
+            _commentParamsServer = commentParamsServer;
+            _irritatedGaugeValue = irritatedGaugeValue;
+        }
+
+        void Update()
+        {
+            if (IsActive) _irritatedGaugeValue.IncreaseValue(_commentParamsServer.GetIrritationIncreasePerSec() * Time.deltaTime);
+        }
+
+        public void SetParameter(string commentWord)
+        {
+            commentText.text = commentWord;
+        }
+
+        public void Daipaned()
+        {
+            IsActive = false;
+            // DaipanedSequence();
+        }
+
+        // void DaipanedSequence()
+        // {
+        //     var prePosition = commentText.transform.position;
+        //     var sequence = DOTween.Sequence()
+        //         .Append(commentText.transform.DOScaleY(0.3f, 0.2f).SetEase(Ease.InQuint)) // 縮めて、
+        //         .Join(commentText.transform.DOMoveY(prePosition.y - 0.6f, 0.2f).SetEase(Ease.InQuint)) // 同時に下に移動
+        //         .Append(commentText.transform.DOScaleY(1.1f, 0.15f).SetEase(Ease.InCubic)) // 素早く大きくする
+        //         .Join(commentText.transform.DOMoveY(prePosition.y + 0.6f, 0.15f).SetEase(Ease.InCubic)) // 同時に上に移動
+        //         .Append(commentText.transform.DOScaleY(0, 0.4f).SetEase(Ease.InCubic)) // 小さくしながら
+        //         .Join(commentText.transform.DOMoveY(prePosition.y - 1, 0.4f).SetEase(Ease.InCubic)) // 同時に下に移動
+        //         .OnComplete(() =>
+        //         {
+        //             _antiCommentCluster.Remove(this);
+        //             Destroy(gameObject);
+        //         });
+        //     sequence.Play();
+        // }
+    }
+}
