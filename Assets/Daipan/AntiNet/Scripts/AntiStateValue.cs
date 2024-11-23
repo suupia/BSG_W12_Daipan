c5b4d09e80d5bdd4934aeca5b2d6ff4e62a407f8
aya097 <akase097@gmail.com>
2024-11-23 19:09:19 +0900

    add banToNormal state by time

--- Assets/Daipan/AntiNet/Scripts/AntiStateValue.cs
+++ Assets/Daipan/AntiNet/Scripts/AntiStateValue.cs
@@ -1,12 +1,21 @@
 #nullable enable
 
+using System;
+using R3;
+using UnityEngine;
+
 namespace Daipan.AntiNet.Scripts
 {
     public class AntiStateValue
     {
+        // todo 値出し
+        float banTime = 5f;
+        //
+
         public AntiStateEnum AntiStateEnum { get => _antiStateEnum; }
 
         private AntiStateEnum _antiStateEnum;
+        private IDisposable? _banToNormalTimer;
 
         public AntiStateValue()
         {
@@ -17,14 +26,22 @@ namespace Daipan.AntiNet.Scripts
         public void SetNormal()
         {
             _antiStateEnum = AntiStateEnum.Normal;
+
+            _banToNormalTimer?.Dispose();
         }
         public void SetBan()
         {
             _antiStateEnum = AntiStateEnum.BAN;
+
+            _banToNormalTimer?.Dispose();
+            _banToNormalTimer = Observable.Timer(TimeSpan.FromSeconds(banTime))
+                .Subscribe(_ => { SetNormal(); });
         }
         public void SetFever()
         {
             _antiStateEnum = AntiStateEnum.FEVER;
+
+            _banToNormalTimer?.Dispose();
         }
 
     }
