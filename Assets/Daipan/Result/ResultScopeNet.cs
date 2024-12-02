f506122a66d4ada90fca858379ba3c7dc042fdd3
aya097 <akase097@gmail.com>
2024-12-02 09:42:18 +0900

    add result scope net

---
+++ Assets/Daipan/Result/ResultScopeNet.cs
@@ -0,0 +1,37 @@
+using Daipan.Comment.MonoScripts;
+using Daipan.Comment.Scripts;
+using Daipan.Core;
+using Daipan.Core.Interfaces;
+using Daipan.Daipan;
+using Daipan.Enemy.Interfaces;
+using Daipan.Enemy.MonoScripts;
+using Daipan.Enemy.Scripts;
+using Daipan.LevelDesign.Comment.Scripts;
+using Daipan.LevelDesign.Enemy.Scripts;
+using Daipan.Player.LevelDesign.Scripts;
+using Daipan.LevelDesign.Tower.Scripts;
+using Daipan.Player.Scripts;
+using Daipan.Stream.MonoScripts;
+using Daipan.Stream.Scripts;
+using Daipan.Stream.Scripts.Utility;
+using Daipan.Tower.Scripts;
+using Daipan.Utility.Scripts;
+using Daipan.Option.MonoScripts;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+using Daipan.Network.MonoScripts;
+
+public sealed class ResultScopeNet : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        // builder.RegisterComponentInHierarchy<TitleGetInputMonoOnline>();
+
+        // // Option
+        // DaipanScope.RegisterOption(builder);
+
+        // // Input
+        // DaipanScope.RegisterInputSerial(builder);
+    }
+}
