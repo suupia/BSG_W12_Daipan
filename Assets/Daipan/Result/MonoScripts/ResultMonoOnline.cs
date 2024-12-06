f660ce4c8997db6be2c0e8b635f32ae62dc26147
suupia <suupia771@gmail.com>
2024-12-06 11:13:21 +0900

    Refactor NetworkDisposerNet

--- Assets/Daipan/Result/MonoScripts/ResultMonoOnline.cs
+++ Assets/Daipan/Result/MonoScripts/ResultMonoOnline.cs
@@ -28,11 +28,12 @@ namespace Daipan.Result.MonoScripts
 
     public static class NetworkPlayerResultHolder
     {
-        public static NetworkPlayerResultEnum NetworkPlayerResultEnum { get; set; } = NetworkPlayerResultEnum.Quit;
-    }
+        public static NetworkPlayerResultEnum NetworkPlayerResultEnum { get; set; } = NetworkPlayerResultEnum.None;
+}
 
     public enum NetworkPlayerResultEnum
     {
+        None,
         Win,
         Lose,
         Quit,
