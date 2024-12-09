72ea90f7a3257bc2e72c0fc73c2ba2fbd4b79100
suupia <suupia771@gmail.com>
2024-12-09 23:09:33 +0900

    Add EnemyViewNet and AbstractEnemyViewNet.cs

---
+++ Assets/Daipan/Enemy/Interfaces/AbstractEnemyViewNet.cs
@@ -0,0 +1,22 @@
+#nullable enable
+using System;
+using Daipan.Enemy.Scripts;
+using Daipan.LevelDesign.Enemy.Scripts;
+using Fusion;
+using UnityEngine;
+
+namespace Daipan.Enemy.Interfaces
+{
+    // SerializeFieldで受け取れるように抽象クラスとした
+    public abstract class AbstractEnemyViewNet : NetworkBehaviour, IEnemyViewMono , IEnemyViewMonoSetDomain 
+    {
+        public abstract void SetDomain(IEnemyViewParamData enemyParamData);
+        public abstract void SetHpGauge(double currentHp, int maxHp);
+        public abstract void Move();
+        public abstract void Attack();
+        public abstract void Died(Action onDied);
+        public abstract void Daipaned(Action onDaipaned);
+        public abstract void Highlight(bool isHighlighted);
+
+    }
+}
---
+++ Assets/Daipan/Enemy/MonoScripts/EnemyViewNet.cs
@@ -0,0 +1,102 @@
+#nullable enable
+using Daipan.Enemy.Interfaces;
+using Daipan.Enemy.Scripts;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Daipan.Enemy.MonoScripts
+{
+    public sealed class EnemyViewNet : AbstractEnemyViewNet, IGetAbstractEnemyViewMono
+    {
+        public AbstractEnemyViewMono GetAbstractEnemyViewMono() => _selectedEnemyViewMono;
+        [SerializeField] EnemyNormalViewMono enemyNormalViewMono = null!;
+        [SerializeField] EnemyBoss1ViewMono enemyBoss1ViewMono = null!; // Tank
+        [SerializeField] EnemyBoss2ViewMono enemyBoss2ViewMono = null!; // 筋肉
+        [SerializeField] EnemyBoss3ViewMono enemyBoss3ViewMono = null!; // 素早い
+        [SerializeField] EnemySpecialViewMono enemySpecialViewMono = null!;
+        [SerializeField] EnemyTotem2ViewMono enemyTotem2ViewMono = null!;
+        [SerializeField] EnemyTotem3ViewMono enemyTotem3ViewMono = null!;
+        AbstractEnemyViewMono _selectedEnemyViewMono = null!;
+
+        public override void SetDomain(IEnemyViewParamData enemyParamData)
+        {
+            Debug.Log("SetDomain enemy enum: " + enemyParamData.GetEnemyEnum());
+            SwitchEnemyView(enemyParamData.GetEnemyEnum());
+            _selectedEnemyViewMono.SetDomain(enemyParamData);
+        }
+        
+        void SwitchEnemyView(EnemyEnum enemyEnum)
+        {
+            enemyNormalViewMono.gameObject.SetActive(false);
+            enemyBoss1ViewMono.gameObject.SetActive(false);
+            enemyBoss2ViewMono.gameObject.SetActive(false);
+            enemyBoss3ViewMono.gameObject.SetActive(false);
+            enemySpecialViewMono.gameObject.SetActive(false);
+            enemyTotem2ViewMono.gameObject.SetActive(false);
+            enemyTotem3ViewMono.gameObject.SetActive(false);
+
+            switch (enemyEnum)
+            {
+                case EnemyEnum.YellowBoss:
+                    enemyBoss1ViewMono.gameObject.SetActive(true);
+                    _selectedEnemyViewMono = enemyBoss1ViewMono;
+                    break;
+                case EnemyEnum.RedBoss:
+                    enemyBoss2ViewMono.gameObject.SetActive(true);
+                    _selectedEnemyViewMono = enemyBoss2ViewMono;
+                    break;
+                case EnemyEnum.BlueBoss:
+                    enemyBoss3ViewMono.gameObject.SetActive(true);
+                    _selectedEnemyViewMono = enemyBoss3ViewMono;
+                    break;
+                case EnemyEnum.SpecialRed:
+                case EnemyEnum.SpecialBlue:
+                case EnemyEnum.SpecialYellow:
+                    enemySpecialViewMono.gameObject.SetActive(true);
+                    _selectedEnemyViewMono = enemySpecialViewMono;
+                    break;
+                case EnemyEnum.Totem2:
+                    enemyTotem2ViewMono.gameObject.SetActive(true);
+                    _selectedEnemyViewMono = enemyTotem2ViewMono;
+                    break;
+                case EnemyEnum.Totem3:
+                    enemyTotem3ViewMono.gameObject.SetActive(true);
+                    _selectedEnemyViewMono = enemyTotem3ViewMono;
+                    break;
+                default:
+                    enemyNormalViewMono.gameObject.SetActive(true);
+                    _selectedEnemyViewMono = enemyNormalViewMono;
+                    break;
+            }
+        }
+        public override void SetHpGauge(double currentHp, int maxHp)
+        {
+            _selectedEnemyViewMono.SetHpGauge(currentHp, maxHp);
+        }
+
+        public override void Move()
+        {
+            _selectedEnemyViewMono.Move();
+        }
+
+        public override void Attack()
+        {
+            _selectedEnemyViewMono.Attack();
+        }
+
+        public override void Died(System.Action onDied)
+        {
+            _selectedEnemyViewMono.Died(onDied);
+        }
+
+        public override void Daipaned(System.Action onDaipaned)
+        {
+            _selectedEnemyViewMono.Daipaned(onDaipaned);
+        }
+
+        public override void Highlight(bool isHighlighted)
+        {
+            _selectedEnemyViewMono.Highlight(isHighlighted);
+        }
+    }
+}
