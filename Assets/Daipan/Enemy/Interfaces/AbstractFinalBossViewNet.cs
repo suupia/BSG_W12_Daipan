a2532ebded8112431a68b6412c5ddc09b9846519
suupia <suupia771@gmail.com>
2024-12-10 14:07:17 +0900

    Add FinalBossViewNet

---
+++ Assets/Daipan/Enemy/Interfaces/AbstractFinalBossViewNet.cs
@@ -0,0 +1,23 @@
+#nullable enable
+using System;
+using Fusion;
+using UnityEngine;
+
+namespace Daipan.Enemy.Interfaces
+{
+    public abstract class AbstractFinalBossViewNet : NetworkBehaviour, IFinalBossViewMono , IFinalBossViewMonoSetDomain
+    {
+        public abstract void SetDomain(IFinalBossViewParamData finalBossViewParamData);
+        public abstract void SetHpGauge(double currentHp, int maxHp);
+        public abstract void Move();
+        public abstract void Attack();
+        public abstract void Died(Action onDied);
+        public abstract void Daipaned(Action onDaipaned);
+        public abstract void DaipanHit();
+        public abstract void Highlight(bool isHighlighted);
+        
+        // FinalBoss
+        public abstract void SummonEnemy();
+    } 
+}
+
