cf8ed2a8d8c6a8c4ed63176e2ed7f6915593fe84
aya097 <akase097@gmail.com>
2024-12-11 11:47:40 +0900

    add Difference in ViewerNumber

--- Assets/Daipan/Daipan/DaipanScope.cs
+++ Assets/Daipan/Daipan/DaipanScope.cs
@@ -206,6 +206,7 @@ namespace Daipan.Daipan
             // Viewer
             builder.RegisterComponentInHierarchy<ViewerViewMono>();
             builder.Register<ViewerPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<ViewerDifferenceViewMono>>();
+            builder.Register<ViewerDifferenceSpawner>(Lifetime.Scoped);
 
             // View
             builder.RegisterComponentInHierarchy<StreamerViewMono>().AsSelf().As<IUpdate>();
