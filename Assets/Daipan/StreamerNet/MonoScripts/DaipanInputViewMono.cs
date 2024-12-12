039d14c9a2fc37307add7073fe159ac85d7d9eb5
aya097 <akase097@gmail.com>
2024-12-12 16:05:52 +0900

    add DaipanInputViewMono

--- Assets/Daipan/StreamerNet/MonoScripts/DaipanInputViewMono.cs
+++ Assets/Daipan/StreamerNet/MonoScripts/DaipanInputViewMono.cs
@@ -1,12 +1,76 @@
 #nullable enable
-
-
+using Daipan.Stream.Interfaces;
+using Daipan.Stream.Scripts;
+using DG.Tweening;
+using R3;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+using VContainer;
 
 namespace Daipan.StreamerNet.MonoScripts
 {
     public class DaipanInputViewMono : MonoBehaviour
     {
+        [SerializeField] Image wordImage = null!;
+        [SerializeField] Image headImage = null!;
+        [SerializeField] Image handImage = null!;
+
+
+        private Sequence? _sequence = null!;
+
+        [SerializeField] bool testFlag;
+        [Inject]
+        public void Initialize(
+            IIrritatedGaugeValue irritatedGaugeValue
+        )
+        {
+            HideView();
+            Observable.EveryValueChanged(irritatedGaugeValue, x => x.IsFull)
+                .Subscribe(_ =>
+                {
+                    if (irritatedGaugeValue.IsFull)
+                    {
+                        ShowView();
+                    }
+                    else
+                    {
+                        HideView();
+                    }
+                })
+                .AddTo(this);
+        }
+
+        void Update()
+        {
+            if (testFlag)
+            {
+                ShowView();
+            }
+            else
+            {
+                HideView();
+            }
+        }
+
+        void ShowView()
+        {
+            wordImage.gameObject.SetActive(true);
+            headImage.gameObject.SetActive(true);
+            handImage.gameObject.SetActive(true);
+            _sequence = DOTween.Sequence();
+
+
+
+        }
+        void HideView()
+        {
+            wordImage.gameObject.SetActive(false);
+            headImage.gameObject.SetActive(false);
+            handImage.gameObject.SetActive(false);
+
+            _sequence?.Kill();
+        }
 
     }
 }
