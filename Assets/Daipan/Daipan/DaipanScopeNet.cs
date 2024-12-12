980b6fe15a629f8af3b13e1c694514130bb68d0f
aya097 <akase097@gmail.com>
2024-12-12 15:25:39 +0900

    change streamer input back

--- Assets/Daipan/Daipan/DaipanScopeNet.cs
+++ Assets/Daipan/Daipan/DaipanScopeNet.cs
@@ -242,6 +242,8 @@ namespace Daipan.Daipan
         {
             // Viewer
             builder.RegisterComponentInHierarchy<ViewerViewMono>();
+            builder.Register<ViewerPrefabLoader>(Lifetime.Scoped).As<IPrefabLoader<ViewerDifferenceViewMono>>();
+            builder.Register<ViewerDifferenceSpawner>(Lifetime.Scoped);
 
             // View
             builder.RegisterComponentInHierarchy<StreamerViewMono>().AsSelf().As<IUpdate>();
