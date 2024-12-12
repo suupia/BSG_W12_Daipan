5ac4ea5fbd2da9cdb6ba3c2f1dbce2c853d65028
aya097 <akase097@gmail.com>
2024-12-12 17:16:52 +0900

    add daipan position

--- Assets/Daipan/StreamerNet/MonoScripts/DaipanInputViewMono.cs
+++ Assets/Daipan/StreamerNet/MonoScripts/DaipanInputViewMono.cs
@@ -2,6 +2,7 @@
 using Daipan.Stream.Interfaces;
 using Daipan.Stream.Scripts;
 using DG.Tweening;
+using ExitGames.Client.Photon.StructWrapping;
 using R3;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
@@ -16,6 +17,15 @@ namespace Daipan.StreamerNet.MonoScripts
         [SerializeField] Image headImage = null!;
         [SerializeField] Image handImage = null!;
 
+        [SerializeField] Vector2 wordPosition;
+        [SerializeField] Vector2 headPosition;
+        [SerializeField] Vector2 handPosition;
+        [SerializeField] Vector2 wordMove;
+        [SerializeField] Vector2 headMove;
+        [SerializeField] Vector2 handMove;
+        [SerializeField] float duration;
+
+
 
         private Sequence? _sequence = null!;
 
@@ -25,7 +35,13 @@ namespace Daipan.StreamerNet.MonoScripts
             IIrritatedGaugeValue irritatedGaugeValue
         )
         {
-            HideView();
+            wordImage.gameObject.SetActive(false);
+            headImage.gameObject.SetActive(false);
+            handImage.gameObject.SetActive(false);
+            wordPosition = wordImage.GetComponent<RectTransform>().anchoredPosition;
+            headPosition = headImage.GetComponent<RectTransform>().anchoredPosition;
+            handPosition = handImage.GetComponent<RectTransform>().anchoredPosition;
+
             Observable.EveryValueChanged(irritatedGaugeValue, x => x.IsFull)
                 .Subscribe(_ =>
                 {
@@ -39,18 +55,8 @@ namespace Daipan.StreamerNet.MonoScripts
                     }
                 })
                 .AddTo(this);
-        }
+            HideView();
 
-        void Update()
-        {
-            if (testFlag)
-            {
-                ShowView();
-            }
-            else
-            {
-                HideView();
-            }
         }
 
         void ShowView()
@@ -58,18 +64,77 @@ namespace Daipan.StreamerNet.MonoScripts
             wordImage.gameObject.SetActive(true);
             headImage.gameObject.SetActive(true);
             handImage.gameObject.SetActive(true);
+
+            _sequence?.Kill();
             _sequence = DOTween.Sequence();
 
-
-
+            _sequence.Append(
+                wordImage.GetComponent<RectTransform>().DOAnchorPos(wordPosition, 0.5f)
+                .SetEase(Ease.Linear)
+            )
+            .Join(
+                headImage.GetComponent<RectTransform>().DOAnchorPos(headPosition, 0.5f)
+                .SetEase(Ease.Linear)
+            )
+            .Join(
+                handImage.GetComponent<RectTransform>().DOAnchorPos(handPosition, 0.5f)
+                .SetEase(Ease.Linear)
+            ).OnComplete(() =>
+            {
+                AnimationView();
+            });
+        }
+        void AnimationView()
+        {
+            _sequence?.Kill();
+            _sequence = DOTween.Sequence();
+            _sequence
+                .Append(
+                    wordImage.GetComponent<RectTransform>().DOAnchorPos(wordPosition + wordMove, duration)
+                    .SetEase(Ease.InQuart)
+                )
+                .Join(
+                    headImage.GetComponent<RectTransform>().DOAnchorPos(headPosition + headMove, duration)
+                    .SetEase(Ease.InQuart)
+                )
+                .Join(
+                    handImage.GetComponent<RectTransform>().DOAnchorPos(handPosition + handMove, duration)
+                    .SetEase(Ease.InQuart)
+                )
+                .Append(
+                    wordImage.GetComponent<RectTransform>().DOAnchorPos(wordPosition, duration * 1.5f)
+                    .SetEase(Ease.Linear)
+                )
+                .Join(
+                    headImage.GetComponent<RectTransform>().DOAnchorPos(headPosition, duration * 1.5f)
+                    .SetEase(Ease.Linear)
+                )
+                .Join(
+                    handImage.GetComponent<RectTransform>().DOAnchorPos(handPosition, duration * 1.5f)
+                    .SetEase(Ease.Linear)
+                )
+                .SetLoops(-1, LoopType.Restart);
         }
         void HideView()
         {
-            wordImage.gameObject.SetActive(false);
-            headImage.gameObject.SetActive(false);
-            handImage.gameObject.SetActive(false);
-
             _sequence?.Kill();
+            _sequence.Append(
+                wordImage.GetComponent<RectTransform>().DOAnchorPos(wordPosition + new Vector2(0, 2000f), 0.5f)
+                .SetEase(Ease.Linear)
+            )
+            .Join(
+                headImage.GetComponent<RectTransform>().DOAnchorPos(headPosition + new Vector2(0, 2000f), 0.5f)
+                .SetEase(Ease.Linear)
+            )
+            .Join(
+                handImage.GetComponent<RectTransform>().DOAnchorPos(handPosition + new Vector2(-2000, 0), 0.5f)
+                .SetEase(Ease.Linear)
+            ).OnComplete(() =>
+            {
+                wordImage.gameObject.SetActive(false);
+                headImage.gameObject.SetActive(false);
+                handImage.gameObject.SetActive(false);
+            });
         }
 
     }
