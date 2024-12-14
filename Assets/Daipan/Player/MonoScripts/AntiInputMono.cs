31b0770f63bd5a1bc252508fdbec209dc15b8f60
aya097 <akase097@gmail.com>
2024-12-15 01:11:54 +0900

    add on submit

--- Assets/Daipan/Player/MonoScripts/AntiInputMono.cs
+++ Assets/Daipan/Player/MonoScripts/AntiInputMono.cs
@@ -85,12 +85,6 @@ namespace Daipan.Player.MonoScripts
                 _antiCommentInputViewMono.CloseChat();
                 _antiCommentSpawnerNetwork.SpawnAntiComment(antiCommentInput.text);
                 antiCommentInput.text = "";
-
-                // if (EventSystem.current.currentSelectedGameObject != null)
-                // {
-                //     EventSystem.current.SetSelectedGameObject(null);
-                // }
-                // antiCommentInput.DeactivateInputField(false);
             });
             // Enterを押して入力終了
             antiCommentInput.onSubmit.AddListener(_ =>
